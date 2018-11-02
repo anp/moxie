17de533ebf082a5f75e941a6a1d3a813b9535e39
Adam Perry <adam.n.perry@gmail.com>
2018-11-02 16:19:01 -0700

    get the web demo harness actually working
    still need to sort out why inotify is being poopy

--- only-web/crate/src/lib.rs
+++ only-web/crate/src/lib.rs
@@ -1,7 +1,11 @@
 #[macro_use]
 extern crate cfg_if;
 
+extern crate only;
 extern crate wasm_bindgen;
+extern crate web_sys;
+
+use only::*;
 use wasm_bindgen::prelude::*;
 
 cfg_if! {
@@ -23,36 +27,15 @@ cfg_if! {
     }
 }
 
-// Definitions of the functionality available in JS, which wasm-bindgen will
-// generate shims for today (and eventually these should be near-0 cost!)
-//
-// These definitions need to be hand-written today but the current vision is
-// that we'll use WebIDL to generate this `extern` block into a crate which you
-// can link and import. There's a tracking issue for this at
-// https://github.com/rustwasm/wasm-bindgen/issues/42
-//
-// In the meantime these are written out by hand and correspond to the names and
-// signatures documented on MDN, for example
-#[wasm_bindgen]
-extern "C" {
-    type HTMLDocument;
-    static document: HTMLDocument;
-    #[wasm_bindgen(method)]
-    fn createElement(this: &HTMLDocument, tagName: &str) -> Element;
-    #[wasm_bindgen(method, getter)]
-    fn body(this: &HTMLDocument) -> Element;
-
-    type Element;
-    #[wasm_bindgen(method, setter = innerHTML)]
-    fn set_inner_html(this: &Element, html: &str);
-    #[wasm_bindgen(method, js_name = appendChild)]
-    fn append_child(this: &Element, other: Element);
-}
-
 // Called by our JS entry point to run the example
 #[wasm_bindgen]
 pub fn run() {
-    let val = document.createElement("p");
-    val.set_inner_html("Hello from Rust, WebAssembly, and Webpack");
-    document.body().append_child(val);
+    let document = web_sys::window().unwrap().document().unwrap();
+    let body = document.body().unwrap();
+    let body: &web_sys::Node = body.as_ref();
+
+    let val = document.create_element("p").unwrap();
+    val.set_inner_html("spoooooky crisp");
+
+    body.append_child(val.as_ref()).unwrap();
 }
