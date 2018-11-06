90ec486a1f2e4d3a31d86670e0128a3d540cd0cb
Adam Perry <adam.n.perry@gmail.com>
2018-11-05 18:42:32 -0800

    Render some basic elements, no listeners yet.

--- only-web/crate/src/lib.rs
+++ only-web/crate/src/lib.rs
@@ -1,12 +1,16 @@
 #[macro_use]
 extern crate cfg_if;
 
+extern crate maplit;
 extern crate only;
 extern crate wasm_bindgen;
 extern crate web_sys;
 
-use only::*;
+use std::collections::BTreeMap;
+
+use maplit::*;
 use wasm_bindgen::prelude::*;
+use web_sys::{Document, Node};
 
 cfg_if! {
     // When the `console_error_panic_hook` feature is enabled, we can call the
@@ -34,8 +38,96 @@ pub fn run() {
     let body = document.body().unwrap();
     let body: &web_sys::Node = body.as_ref();
 
-    let val = document.create_element("p").unwrap();
-    val.set_inner_html("spoooooky crisp");
+    let element = elements::Standard {
+        ty: "div".to_string(),
+        children: vec![
+            Box::new(elements::Standard {
+                ty: "input".to_string(),
+                props: btreemap! {
+                    "value".to_string() => "foo".to_string(),
+                    "type".to_string() => "text".to_string(),
+                },
+                ..Default::default()
+            }),
+            Box::new(elements::Standard {
+                ty: "a".to_string(),
+                props: btreemap! {
+                    "href".to_string() => "/bar".to_string(),
+                },
+                ..Default::default()
+            }),
+            Box::new(elements::Standard {
+                ty: "span".to_string(),
+                children: vec![Box::new(elements::Text {
+                    contents: "hello world".to_string(),
+                })],
+                ..Default::default()
+            }),
+        ],
+        props: btreemap! {
+            "id".to_string() => "container".to_string(),
+        },
+        ..Default::default()
+    };
+    element.render(&document, body);
+}
 
-    body.append_child(val.as_ref()).unwrap();
+pub trait Element {
+    fn render(&self, document: &Document, parent_dom: &Node);
+}
+
+mod elements {
+    use super::*;
+    use web_sys::Event;
+
+    #[derive(Default)]
+    pub struct Standard {
+        pub ty: String,
+        pub children: Vec<Box<dyn Element>>,
+        pub props: BTreeMap<String, String>,
+        pub listeners: BTreeMap<String, Box<dyn Fn(Event)>>,
+    }
+
+    impl Element for Standard {
+        fn render(&self, document: &Document, parent_dom: &Node) {
+            // Create DOM element
+            let dom = document.create_element(&self.ty).unwrap();
+            // Add event listeners
+
+            // for (event_type, listener) in self.listeners {
+            //     let eventable = EventableDomNode(dom);
+            //     eventable.addEventListener(&event_type, Closure::wrap(listener.clone()));
+            //     dom = eventable.0;
+            // }
+            //   const isListener = name => name.startsWith("on");
+            //   Object.keys(props).filter(isListener).forEach(name => {
+            //     const eventType = name.toLowerCase().substring(2);
+            //     dom.addEventListener(eventType, props[name]);
+            //   });
+
+            for (property, value) in &self.props {
+                dom.set_attribute(&property, &value).unwrap();
+            }
+
+            let dom_node: Node = dom.into();
+            self.children
+                .iter()
+                .for_each(|child| child.render(document, &dom_node));
+
+            parent_dom.append_child(&dom_node).unwrap();
+        }
+    }
+
+    pub struct Text {
+        pub contents: String,
+    }
+
+    impl Element for Text {
+        fn render(&self, document: &Document, parent_dom: &Node) {
+            let text: Node = document.create_text_node(&self.contents).into();
+
+            // TODO make this the responsibility of the "renderer"
+            parent_dom.append_child(&text).unwrap();
+        }
+    }
 }
