f215cc4a02da90375d144389c2a59d2d9fbf78c2
Adam Perry <adam.n.perry@gmail.com>
2019-04-21 11:41:58 -0700

    [dom] give example a diff filenam (editors!)

---
+++ moxie-dom/examples/hacking/crate/src/hacking.rs
@@ -0,0 +1,29 @@
+use moxie_dom::greet;
+use wasm_bindgen::prelude::*;
+
+// Called by our JS entry point to run the example.
+#[wasm_bindgen]
+pub fn run() -> Result<(), JsValue> {
+    set_panic_hook();
+
+    let window = web_sys::window().expect("should have a Window");
+    let document = window.document().expect("should have a Document");
+
+    let p: web_sys::Node = document.create_element("p")?.into();
+    p.set_text_content(Some("Hello from Rust, WebAssembly, and Webpack!"));
+
+    let body = document.body().expect("should have a body");
+    let body: &web_sys::Node = body.as_ref();
+    body.append_child(&p)?;
+
+    greet();
+
+    Ok(())
+}
+
+fn set_panic_hook() {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function to get better error messages if we ever panic.
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+}
