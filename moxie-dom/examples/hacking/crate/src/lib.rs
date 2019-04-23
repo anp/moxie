4be3ba542b606bc26370f3fe256cb56a0613555d
Adam Perry <adam.n.perry@gmail.com>
2019-04-23 16:36:35 -0700

    [dom] hacking example that seems to mostly run

--- moxie-dom/examples/hacking/crate/src/lib.rs
+++ moxie-dom/examples/hacking/crate/src/lib.rs
@@ -1,7 +1,5 @@
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
+pub mod hacking;
+
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-mod hacking;
