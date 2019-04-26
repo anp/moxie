d60e59310fe3f6e35c0d901090c6252dbee7d87c
Adam Perry <adam.n.perry@gmail.com>
2019-04-25 23:28:05 -0700

    [dom] compose future is running on the web!!!!

--- moxie-dom/src/lib.rs
+++ moxie-dom/src/lib.rs
@@ -1,44 +1,54 @@
 pub mod prelude {
-    pub use moxie::{self, *};
+    pub use {
+        crate::{DomBinding, WebSpawner},
+        moxie::{self, *},
+    };
 }
 
-use crate::prelude::*;
+use {
+    crate::prelude::*,
+    futures::{
+        future::{FutureExt, LocalFutureObj, TryFutureExt},
+        task::SpawnError,
+    },
+    stdweb::*,
+};
 
-// pub struct WebSpawner;
+pub struct WebSpawner;
 
-// impl PrioritySpawn for WebSpawner {
-//     fn spawn_local(&mut self, future: LocalFutureObj<'static, ()>) -> Result<(), SpawnError> {
-//         wasm_bindgen_futures::spawn_local(future.unit_error().compat());
-//         Ok(())
-//     }
+impl PrioritySpawn for WebSpawner {
+    fn spawn_local(&mut self, future: LocalFutureObj<'static, ()>) -> Result<(), SpawnError> {
+        wasm_bindgen_futures::spawn_local(future.unit_error().compat());
+        Ok(())
+    }
 
-//     fn child(&self) -> Box<dyn PrioritySpawn> {
-//         Box::new(WebSpawner)
-//     }
-// }
+    fn child(&self) -> Box<dyn PrioritySpawn> {
+        Box::new(WebSpawner)
+    }
+}
 
-// #[props]
-// pub struct DomBinding<Root: Component> {
-//     pub node: web::Node,
-//     pub root: Root,
-// }
+#[props]
+pub struct DomBinding<Root: Component> {
+    pub node: web::Node,
+    pub root: Root,
+}
 
-// impl<Root> Component for DomBinding<Root>
-// where
-//     Root: Component,
-// {
-//     fn compose(scp: Scope, Self { node, root }: Self) {
-//         let child_id = scope!(scp.id());
-//         let child_scope = scp.child(child_id);
-//         // child_scope.install_witness(Weaver::attached_to(node));
+impl<Root> Component for DomBinding<Root>
+where
+    Root: Component,
+{
+    fn compose(scp: Scope, Self { node, root }: Self) {
+        let child_id = scope!(scp.id());
+        let child_scope = scp.child(child_id);
+        // child_scope.install_witness(Weaver::attached_to(node));
 
-//         scp.compose_child(child_id, root);
+        scp.compose_child(child_id, root);
 
-//         // let weaver: Weaver = child_scope.remove_witness().unwrap();
+        // let weaver: Weaver = child_scope.remove_witness().unwrap();
 
-//         // TODO make all the nodes go together?
-//     }
-// }
+        // TODO make all the nodes go together?
+    }
+}
 
 // #[props]
 // pub struct Span {}
