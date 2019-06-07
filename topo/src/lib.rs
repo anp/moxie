4161ad15973a2eb44519ac5132649d1cc6c8ce5f
Adam Perry <adam.n.perry@gmail.com>
2019-06-07 06:50:27 -0700

    make a looping example work!!!
    also move the internals-reliant tests back to unit tests from the integration binary

--- topo/src/lib.rs
+++ topo/src/lib.rs
@@ -5,47 +5,7 @@ pub use topo_macro::topo;
 
 use std::{any::TypeId, cell::RefCell, hash::Hash};
 
-/// Identifies a dynamic scope within the call topology.
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct Point {
-    count_of_current: usize,
-    callsites: im::Vector<TypeId>,
-}
-
-static_assertions::assert_impl!(pt; Point, Clone, Hash, Eq, Send, Sync);
-
-impl Point {
-    /// Returns the `Point` identifying the current dynamic scope.
-    #[inline]
-    pub fn current() -> Self {
-        __CURRENT_POINT.with(|p| p.borrow().clone())
-    }
-
-    /// Enters the dynamic scope of the point and calls the closure provided, returning its value.
-    pub fn enter<T>(self, op: impl FnOnce() -> T) -> T {
-        let prev = Some(__CURRENT_POINT.with(|p| p.replace(self)));
-        let _drop_when_out_of_scope_pls = PointGuardLol { prev };
-        op()
-    }
-
-    /// Creates the next "link" in the chain of IDs which represents our path to the current Point.
-    #[inline]
-    #[doc(hidden)]
-    pub fn __child(&self, callsite: TypeId) -> Self {
-        let mut child = self.clone();
-
-        if child.callsites.back() == Some(&callsite) {
-            child.count_of_current += 1;
-        } else {
-            child.count_of_current = 0;
-            child.callsites.push_back(callsite);
-        }
-
-        child
-    }
-}
-
-/// Calls the provided expression within its `Point`.
+/// Calls the provided expression within a `Point` bound to the callsite.
 ///
 /// ```
 /// topo::call!(|| println!("{:?}", topo::Point::current()));
@@ -53,8 +13,81 @@ impl Point {
 #[macro_export]
 macro_rules! call {
     ($inner:expr) => {
-        $crate::__point!().enter($inner)
+        $crate::Point::__enter_child($crate::__point_id!(), $inner)
     };
+}
+
+/// Identifies a dynamic scope within the call topology.
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Point {
+    current: Callsite,
+    path: im::Vector<Callsite>,
+    prev_sibling: Option<Callsite>,
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub struct Callsite {
+    ty: TypeId,
+    count: usize,
+}
+
+impl Callsite {
+    fn new(ty: TypeId, prev_sibling: &Option<Callsite>) -> Self {
+        let prev_count = match prev_sibling {
+            Some(ref prev) if prev.ty == ty => prev.count,
+            _ => 0,
+        };
+
+        Self {
+            ty,
+            count: prev_count + 1,
+        }
+    }
+}
+
+static_assertions::assert_impl!(pt; Point, Clone, Hash, Eq, Send, Sync);
+
+impl Point {
+    /// Returns the `Point` identifying the current dynamic scope.
+    #[inline]
+    #[doc(hidden)]
+    pub fn current() -> Self {
+        __CURRENT_POINT.with(|p| p.borrow().clone())
+    }
+
+    /// Creates the next "link" in the chain of IDs which represents our path to the current Point.
+    #[inline]
+    #[doc(hidden)]
+    pub fn __enter_child<T>(callsite_ty: TypeId, op: impl FnOnce() -> T) -> T {
+        struct PointGuardLol {
+            prev: Option<Point>,
+        }
+
+        impl Drop for PointGuardLol {
+            #[inline]
+            fn drop(&mut self) {
+                __CURRENT_POINT.with(|p| p.replace(self.prev.take().unwrap()));
+            }
+        }
+
+        let _drop_when_out_of_scope_pls = __CURRENT_POINT.with(|p| {
+            let mut p = p.borrow_mut();
+            let current = Callsite::new(callsite_ty, &p.prev_sibling);
+            let mut path = p.path.clone();
+            path.push_back(p.current);
+
+            let child = Self {
+                current,
+                path,
+                prev_sibling: None,
+            };
+            p.prev_sibling = Some(current);
+
+            let prev = Some(std::mem::replace(&mut *p, child));
+            PointGuardLol { prev }
+        });
+        op()
+    }
 }
 
 /// Defines a new macro (named after the first metavariable) which calls a function (named in
@@ -92,28 +125,52 @@ macro_rules! __point_id {
 thread_local! {
     /// The `Point` representing the current dynamic scope.
     pub static __CURRENT_POINT: RefCell<Point> = RefCell::new(Point {
-        count_of_current: 0,
-        callsites: im::Vector::new(),
+        current: Callsite {  count: 1, ty: __point_id!(), },
+        path: im::vector![],
+        prev_sibling: None,
     });
 }
 
-/// Mark a `Point` in the call topology by creating a TypeId unique to the expanded location.
-#[doc(hidden)]
-#[macro_export]
-macro_rules! __point {
-    () => {
-        $crate::Point::current().__child($crate::__point_id!())
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        cell::RefCell,
+        panic::{catch_unwind, AssertUnwindSafe},
     };
-}
 
-/// Resets the current Point to the one stored when the struct is dropped.
-struct PointGuardLol {
-    prev: Option<Point>,
-}
+    #[test]
+    fn one_panicking_child_in_a_loop() {
+        let root = Point::current();
+        assert_eq!(root, Point::current());
 
-impl Drop for PointGuardLol {
-    #[inline]
-    fn drop(&mut self) {
-        __CURRENT_POINT.with(|p| p.replace(self.prev.take().unwrap()));
+        let second_id = __point_id!();
+        let prev = AssertUnwindSafe(RefCell::new(Point::current()));
+
+        assert_eq!(root, Point::current());
+
+        for _ in 0..100 {
+            let called = AssertUnwindSafe(std::cell::Cell::new(false));
+            let res = catch_unwind(|| {
+                Point::__enter_child(second_id, || {
+                    assert_eq!(second_id, Point::current().current.ty);
+                    assert_ne!(
+                        &*prev.borrow(),
+                        &Point::current(),
+                        "entered the same Point twice in this loop"
+                    );
+                    prev.replace(Point::current());
+                    called.set(true);
+                    panic!("checking unwind safety?");
+                });
+            });
+
+            // make sure we've returned to an expected baseline
+            let curr = Point::current();
+            assert_eq!(root.current, curr.current);
+            assert_eq!(root.path, curr.path);
+            assert!(called.get());
+            assert!(res.is_err());
+        }
     }
 }
--- topo/tests/simple.rs
+++ topo/tests/simple.rs
@@ -1,23 +1,6 @@
 use topo::*;
 
 #[test]
-fn one_child() {
-    let root = Point::current();
-
-    let second = __point!();
-    assert_ne!(root, second);
-    assert_eq!(root, Point::current());
-
-    let mut called = false;
-    second.clone().enter(|| {
-        assert_eq!(second, Point::current());
-        called = true;
-    });
-    assert_eq!(root, Point::current());
-    assert!(called);
-}
-
-#[test]
 fn invoke_test_topo() {
     #[topo]
     fn topo_test(prev: Point) {
@@ -26,29 +9,4 @@ fn invoke_test_topo() {
 
     let prev = Point::current();
     topo_test!(prev);
-}
-
-#[test]
-#[ignore]
-fn loops() {
-    unimplemented!()
-}
-
-#[test]
-fn parent_reset_on_recovered_panic() {
-    let root = Point::current();
-
-    let second = __point!();
-    assert_ne!(root, second);
-    assert_eq!(root, Point::current());
-
-    let res = std::panic::catch_unwind(|| {
-        second.clone().enter(|| {
-            assert_eq!(second, Point::current());
-            call!(|| assert_ne!(Point::current(), second));
-            panic!("the second should be unset by this");
-        })
-    });
-    assert_eq!(root, Point::current());
-    assert!(res.is_err());
 }
