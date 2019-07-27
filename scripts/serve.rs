d49213c2d6cbbd4a0a597d8cc6aaa88e5d7b9730
Adam Perry <adam.n.perry@gmail.com>
2019-07-26 20:51:05 -0700

    file watching middleware is stubbed out

--- scripts/serve.rs
+++ scripts/serve.rs
@@ -7,11 +7,13 @@
 //! [dependencies]
 //! actix = "0.8"
 //! actix-files = "0.1"
-//! actix-service = "0.4"
 //! actix-web = "1"
 //! actix-web-actors = "1"
-//! futures-preview = { version = "0.3.0-alpha.17", features = [ "async-await", "compat", "nightly" ] }
+//! crossbeam = "0.7"
+//! futures01 = { version = "0.1", package = "futures" }
+//! futures = { package = "futures-preview", version = "0.3.0-alpha.17", features = [ "async-await", "compat", "nightly" ] }
 //! gumdrop = "0.6"
+//! notify = "5.0.0-pre.1"
 //! parking_lot = "0.9"
 //! pretty_env_logger = "0.3"
 //! tracing = { version = "0.1", features = [ "log" ] }
@@ -20,13 +22,21 @@
 
 use {
     actix::prelude::*,
-    actix_service::ServiceExt,
-    actix_web::{middleware, web, App, HttpServer},
+    actix_web::{
+        dev::{Service, ServiceRequest, ServiceResponse, Transform},
+        middleware, web, App, Error, HttpServer,
+    },
     actix_web_actors::ws,
+    futures::{compat::Compat, future::BoxFuture},
+    // crossbeam::channel::unbounded as chan,
+    futures01::Async,
     gumdrop::Options,
+    // notify::Watcher,
+    // parking_lot::Mutex,
     std::{
         net::IpAddr,
         path::{Path, PathBuf},
+        // sync::Arc,
         time::{Duration, Instant},
     },
     tracing::*,
@@ -52,26 +62,102 @@ fn main() {
     let root_path = Path::new(&scripts_path).parent().unwrap().to_path_buf();
     let config = Config::parse_args_default_or_exit();
 
-    let change_socket = |req, stream: web::Payload| {
-        ws::start(
-            ChangeWatchSession {
-                last_heartbeat: Instant::now(),
-            },
-            &req,
-            stream,
-        )
-    };
-
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
-            .service(web::resource("/ch-ch-ch-changes").route(web::get().to(change_socket)))
-            .service(actix_files::Files::new("/", &root_path).show_files_listing())
+            .service(web::resource("/ch-ch-ch-changes").route(web::get().to(
+                |req, stream: web::Payload| {
+                    ws::start(
+                        ChangeWatchSession {
+                            last_heartbeat: Instant::now(),
+                        },
+                        &req,
+                        stream,
+                    )
+                },
+            )))
+            .wrap(FilesWatcher::new(&root_path))
+            .default_service(actix_files::Files::new("/", &root_path).show_files_listing())
     })
     .bind((config.addr, config.port))
     .unwrap()
     .run()
     .unwrap();
+}
+
+struct FilesWatcher {
+    path: PathBuf,
+    paths_of_interest: Vec<PathBuf>,
+    watcher: notify::RecommendedWatcher,
+    sessions: Vec<actix::Addr<ChangeWatchSession>>,
+    joiner: Option<()>,
+}
+
+impl FilesWatcher {
+    fn new(root_path: &Path) -> Self {
+        unimplemented!()
+
+        // let joiner = self.joiner.as_mut();
+
+        // if joiner.is_none() {
+        //     let (tx, _rx) = chan();
+
+        //     std::thread::spawn(|| {
+        //         let watcher = Arc::new(Mutex::new(
+        //             notify::watcher(tx, std::time::Duration::from_millis(500)).unwrap(),
+        //         ));
+        //         watcher
+        //             .lock()
+        //             .watch(root, notify::RecursiveMode::Recursive)
+        //             .unwrap();
+        //     });
+        // }
+
+        // self.joiner.as_ref().unwrap().0.clone()
+    }
+}
+
+impl<S> Transform<S> for FilesWatcher {
+    type Request = ServiceRequest;
+    type Response = ServiceResponse;
+    type Error = Error;
+    type Transform = WatchHandle;
+    type InitError = ();
+    type Future = Compat<BoxFuture<'static, Result<Self::Transform, Self::InitError>>>;
+    fn new_transform(&self, service: S) -> Self::Future {
+        unimplemented!()
+    }
+}
+
+struct WatchHandle {
+    request: ServiceRequest,
+}
+
+impl WatchHandle {
+    fn add_session(&self, session: Addr<ChangeWatchSession>) {
+        unimplemented!()
+    }
+
+    fn watch_path(&self, path: PathBuf) {
+        unimplemented!()
+    }
+
+    fn start_watching(&self, request: &ServiceRequest) {
+        unimplemented!()
+    }
+}
+
+impl Service for WatchHandle {
+    type Request = ServiceRequest;
+    type Response = ServiceResponse;
+    type Error = Error;
+    type Future = Compat<BoxFuture<'static, Result<Self::Response, Self::Error>>>;
+    fn poll_ready(&mut self) -> Result<Async<()>, Self::Error> {
+        unimplemented!()
+    }
+    fn call(&mut self, req: Self::Request) -> Self::Future {
+        unimplemented!()
+    }
 }
 
 struct ChangeWatchSession {
