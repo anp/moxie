9e008a16a2b069ff28c6396f1c8b58c40941fbdd
Adam Perry <adam.n.perry@gmail.com>
2019-08-20 06:30:39 -0700

    [glutin] refactoring event loop impl for clarity
    thanks clippy cyclomatic complexity lint!

--- guin/glutin/src/lib.rs
+++ guin/glutin/src/lib.rs
@@ -19,51 +19,65 @@ pub fn show_in_window(root: impl Component + Clone + 'static, title: &str) {
     let wb = WindowBuilder::new().with_title(title);
     let windowed_context = ContextBuilder::new().build_windowed(wb, &el).unwrap();
     let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+    windowed_context.swap_buffers().unwrap();
 
     let mut runtime = moxie::Runtime::new(move || show!(root.clone()));
     runtime.set_state_change_waker(el_waker);
 
+    info!("entering glutin event loop");
     el.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
-        let mut should_tick = false;
-        let mut should_swap = false;
-
-        match event {
-            Event::LoopDestroyed => return,
-            Event::WindowEvent { ref event, .. } => match event {
-                WindowEvent::Resized(logical_size) => {
-                    let dpi_factor = windowed_context.window().hidpi_factor();
-                    windowed_context.resize(logical_size.to_physical(dpi_factor));
-                    should_tick = true;
+        let mut tick_runtime = || {
+            let window_size = WindowSize(windowed_context.window().inner_size());
+            debug!({ ?window_size }, "ticking runtime");
+            topo::call! {
+                {
+                    let window_size: &WindowSize = &*topo::Env::expect();
+                    debug!({ ?window_size }, "before calling root");
+                    runtime.run_once();
+                },
+                env! {
+                    WindowSize => window_size,
                 }
-                WindowEvent::RedrawRequested => {
-                    trace!("redraw requested");
-                    should_swap = true;
-                }
-                WindowEvent::CloseRequested => {
-                    info!("exiting");
-                    *control_flow = ControlFlow::Exit;
-                    return;
-                }
-                _ => (),
-            },
-            Event::UserEvent(()) => {
-                debug!("woken by moxie runtime");
-                should_tick = true;
-            }
-            _ => (),
-        }
-
-        if should_tick {
-            runtime.run_once();
-            should_swap = true;
-        }
-
-        if should_swap {
+            };
             windowed_context.swap_buffers().unwrap();
+        };
+
+        let outcome = match event {
+            Event::LoopDestroyed => Outcome::None,
+            Event::WindowEvent { ref event, .. } => match event {
+                WindowEvent::Resized(logical_size) => Outcome::Resize(*logical_size),
+                WindowEvent::RedrawRequested => Outcome::TickRuntime,
+                WindowEvent::CloseRequested => Outcome::Exit,
+                _ => Outcome::None,
+            },
+            Event::UserEvent(()) => Outcome::TickRuntime,
+            _ => Outcome::None,
+        };
+
+        match outcome {
+            Outcome::Resize(new_size) => {
+                let dpi_factor = windowed_context.window().hidpi_factor();
+                windowed_context.resize(new_size.to_physical(dpi_factor));
+                tick_runtime();
+            }
+            Outcome::TickRuntime => tick_runtime(),
+            Outcome::Exit => *control_flow = ControlFlow::Exit,
+            Outcome::None => (),
         }
     });
+}
+
+#[derive(Debug)]
+pub struct WindowSize(pub glutin::dpi::LogicalSize);
+
+#[derive(Debug, PartialEq)]
+enum Outcome {
+    Resize(glutin::dpi::LogicalSize),
+    TickRuntime,
+    Exit,
+    None,
 }
 
 struct GuinWaker(Mutex<EventLoopProxy<()>>);
