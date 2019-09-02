f82e831a1b1f1965597d08b710a589abd8b3e1ee
Adam Perry <adam.n.perry@gmail.com>
2019-09-02 14:22:11 -0700

    [core] Delete nodes.rs and move runtime.rs into the root.

--- src/lib.rs
+++ src/lib.rs
@@ -22,124 +22,151 @@
 
 #[macro_use]
 mod memo;
-mod nodes;
-mod runtime;
 mod state;
 
 #[doc(hidden)]
 pub use topo;
 #[doc(inline)]
-pub use {memo::*, nodes::*, runtime::*, state::*};
+pub use {memo::*, state::*};
 
-use {std::fmt::Debug, tracing::*};
+use {crate::MemoStore, std::task::Waker, tracing::*};
 
-/// TODO explain a component...somehow
-pub trait Component: Debug + Sized {
-    /// Defines the `Component` at a given point in time.
-    ///
-    /// TODO explain "right now" declaration
-    /// TODO explain memoization of this call
-    /// TODO explain show macro
-    fn contents(self);
-}
+/// Revisions measure moxie's notion of time passing. Each [`Runtime`] increments its Revision
+/// on every iteration. [`crate::Commit`]s to state variables are annotated with the Revision
+/// during which they were made.
+#[derive(Clone, Copy, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Revision(pub u64);
 
-impl<I, C> Component for I
-where
-    I: IntoIterator<Item = C> + Debug,
-    C: Component,
-{
-    fn contents(self) {
-        for component in self {
-            show!(component);
+impl Revision {
+    /// Returns the current revision. Will return `Revision(0)` if called outside of a Runtime's
+    /// execution.
+    pub fn current() -> Self {
+        if let Some(r) = topo::Env::get::<Revision>() {
+            *r
+        } else {
+            Revision::default()
         }
     }
 }
 
-#[doc(hidden)]
-#[topo::bound]
-pub fn show_one(component: impl Component) {
-    let show_span = once!(|| trace_span!("show component"));
-    let state_revision = once!(RevisionChain::new);
-
-    let _in_span = show_span.enter();
-    topo::call!(
-        {
-            let rev = state_revision.current();
-            show_span.record("rev", &rev);
-            trace!({ props = ?component }, "showing");
-            component.contents();
-        },
-        env! {
-            RevisionChain => state_revision.clone(),
-        }
-    );
-}
-
-#[macro_export]
-macro_rules! show {
-    ($($child:expr),+) => {
-        $(show_one!($child);)+
-    };
-}
-
-pub trait Parent<Next: Component>: Component {
-    // TODO can we express these automatically in terms of the Self<T> -> Self<SibList<T, Next>> xform?
-    type Output: Component;
-
-    fn child(self, next: Next) -> Self::Output;
-}
-
-pub fn sib_cons<Current, Next>(curr: Current, next: Next) -> SibList<Current, Next> {
-    SibList { curr, next }
-}
-
-#[derive(Debug)]
-pub struct SibList<Current, Next> {
-    curr: Current,
-    next: Next,
-}
-
-impl<Current, Next> Component for SibList<Current, Next>
-where
-    Current: Component,
-    Next: Component,
-{
-    fn contents(self) {
-        show!(self.curr);
-        show!(self.next);
-    }
-}
-
-#[derive(Clone, Copy, Debug, Default)]
-pub struct Empty;
-
-impl Component for Empty {
-    fn contents(self) {}
-}
-
-/// TODO find a better name. A wrapper for a closure which is called as if it were a child component, i.e. in its own topological point.
-pub struct Clomp<F>(pub F);
-
-impl<F> Clomp<F>
-where
-    F: FnOnce(),
-{
-    fn new(op: F) -> Self {
-        Clomp(op)
-    }
-}
-
-impl<F> Component for Clomp<F>
-where
-    F: FnOnce(),
-{
-    fn contents(self) {
-        (self.0)()
-    }
-}
-
-impl<F> std::fmt::Debug for Clomp<F> {
+impl std::fmt::Debug for Revision {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        f.write_fmt(format_args!("Clomp(TODO better output)"))
+        f.write_fmt(format_args!("r{}", self.0))
+    }
+}
+
+/// A `Runtime` is the entry point of the moxie runtime environment. On each invocation of
+/// `run_once`, it calls the root with which it was initialized. Typically this is invoked in a loop
+/// which sleeps until the provided waker is invoked, as is the case in the `Future` implementation.
+/// Usually root closure will cause some memoized side effect to the render environment in order to
+/// produce a view of the input data. A Runtime's root closure will also transitively establish
+/// event handlers, either via locally polled `Future`s or via the containing environment's
+/// callback or event mechanisms.
+///
+/// While the Runtime may iterate very frequently (potentially more than once for any given output
+/// frame), we use [topological memoization](crate::memo) to minimize the code run each time.
+///
+/// See the documentation for [`Runtime::run_once`] for details on the core loop body.
+///
+/// ## Minimal Example
+///
+/// ```
+/// let mut rt = moxie::Runtime::new(|| {});
+/// assert_eq!(rt.revision().0, 0);
+/// for i in 0..10 {
+///     rt.run_once();
+///     assert_eq!(rt.revision(), moxie::Revision(1));
+/// }
+/// ```
+pub struct Runtime<Root> {
+    revision: Revision,
+    store: MemoStore,
+    root: Root,
+    wk: Waker,
+    span: Span,
+}
+
+impl<Root> Runtime<Root>
+where
+    Root: FnMut(),
+{
+    /// Construct a new Runtime at revision 0 and blank storage.
+    pub fn new(root: Root) -> Self {
+        let span = trace_span!("runtime", rev = 0);
+        Self {
+            span,
+            revision: Revision(0),
+            store: MemoStore::default(),
+            root,
+            wk: futures::task::noop_waker(),
+        }
+    }
+
+    /// The current revision of the runtime.
+    pub fn revision(&self) -> Revision {
+        self.revision
+    }
+
+    /// Run a single iteration of the root closure with access to the [`topo::Env`] provided by the
+    /// [`Runtime`] . Increments the [`Revision`] counter of this [`Runtime`] by one.
+    pub fn run_once(&mut self) {
+        self.revision.0 += 1;
+        self.span.record("rev", &self.revision.0);
+        let _entered = self.span.enter();
+
+        topo::root!(
+            (self.root)(),
+            env! {
+                MemoStore => self.store.clone(),
+                Revision => self.revision,
+                RunLoopWaker => RunLoopWaker(self.wk.clone()),
+            }
+        );
+        self.store.gc();
+    }
+
+    /// Sets the [`std::task::Waker`] which will be called when state variables receive commits. By
+    /// default the runtime has a
+    pub fn set_state_change_waker(&mut self, wk: Waker) -> &mut Self {
+        self.wk = wk;
+        self
+    }
+}
+
+/// Responsible for waking the Runtime task. Because the topo environment is namespaced by type,
+/// we create a newtype here so that other crates don't accidentally cause strange behavior by
+/// overriding our access to it.
+#[derive(Clone)]
+pub(crate) struct RunLoopWaker(std::task::Waker);
+
+impl RunLoopWaker {
+    pub(crate) fn wake(&self) {
+        self.0.wake_by_ref();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn propagating_env_to_runtime() {
+        let first_byte = 0u8;
+
+        let mut runtime = Runtime::new(|| {
+            let from_env: u8 = *topo::Env::expect();
+            assert_eq!(from_env, first_byte);
+        });
+
+        assert!(topo::Env::get::<u8>().is_none());
+        topo::call!(
+            {
+                runtime.run_once();
+            },
+            env! {
+                u8 => first_byte,
+            }
+        );
+        assert!(topo::Env::get::<u8>().is_none());
     }
 }
