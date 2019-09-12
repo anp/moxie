61c5503dbae4946551da50e2905977bdc9174e67
Adam Perry <adam.n.perry@gmail.com>
2019-09-12 08:20:48 -0700

    [dom] element can only be declared within a closure passed to element!
    until topo::aware can work on methods, this ensures that attributes and event handlers are correctly attributed

--- dom/examples/todo/src/footer.rs
+++ dom/examples/todo/src/footer.rs
@@ -5,45 +5,45 @@ use {
 
 #[topo::aware]
 pub fn items_remaining(num_active: usize) {
-    element!("span")
+    element!("span", |e| e
         .attr("class", "todo-count")
         // lol this is awful
         .inner(|| {
-            element!("strong").inner(|| {
+            element!("strong", |e| e.inner(|| {
                 if num_active == 0 {
                     text!("No")
                 } else {
                     text!(num_active)
                 }
-            });
+            }));
             text!(format!(
                 " {} left",
                 if num_active == 1 { "item" } else { "items" }
-            ))
-        });
+            ));
+        }));
 }
 
 #[topo::aware]
 pub fn clear_completed_button() {
     let todos = topo::Env::expect::<Key<Vec<Todo>>>();
 
-    element!("button")
+    element!("button", |e| e
         .attr("class", "clear-completed")
         .on(
             |_: ClickEvent, todos| Some(todos.iter().filter(|t| !t.completed).cloned().collect()),
             todos.clone(),
         )
-        .inner(|| text!("Clear completed"));
+        .inner(|| text!("Clear completed")));
 }
 
 #[topo::aware]
 pub fn footer(num_complete: usize, num_active: usize) {
-    element!("footer").attr("class", "footer").inner(|| {
+    element!("footer", |e| e.attr("class", "footer").inner(|| {
         items_remaining!(num_active);
         filter!();
 
         if num_complete > 0 {
             clear_completed_button!();
         }
-    });
+    }));
 }
