abbd13db63236288d674032446515c2527f6ca7f
Adam Perry <adam.n.perry@gmail.com>
2019-09-13 23:32:54 -0700

    [ofl] website/book modules are compiling again

--- ofl/src/website/book.rs
+++ ofl/src/website/book.rs
@@ -1,9 +1,9 @@
 
-use mdbook::MDBook;
+use {failure::{Error, SyncFailure}, mdbook::MDBook, std::path::Path};
 
-fn build_book(book_root: &Path, output_dir: &Path) -> Result<(), Error> {
-    let mut md = MDBook::load(book_root)?;
-    md.build()?;
-    unimplemented!()
+pub fn build_book(book_root: &Path, output_dir: &Path) -> Result<(), Error> {
+    let mut md = MDBook::load(book_root).map_err(SyncFailure::new)?;
+    md.build().map_err(SyncFailure::new)?;
+    unimplemented!();
     Ok(())
 }
