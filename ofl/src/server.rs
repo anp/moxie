27e49a0412efdf4a8ad735948d12a9c380704e6e
Adam Perry <adam.n.perry@gmail.com>
2019-09-14 18:07:52 -0700

    move ChangeWatchSession to session.rs, join threads properly, only set up watchers for successful requests

--- ofl/src/server.rs
+++ ofl/src/server.rs
@@ -12,14 +12,17 @@ use {
     futures01::{Async, Future as OldFuture},
     gumdrop::Options,
     notify::Watcher,
+    session::{ChangeWatchSession, Changed},
     std::{
         net::IpAddr,
         path::{Path, PathBuf},
         sync::Arc,
-        time::{Duration, Instant},
+        thread::JoinHandle,
     },
     tracing::*,
 };
+
+mod session;
 
 #[derive(Debug, Options)]
 pub struct ServerOpts {
@@ -44,7 +47,10 @@ impl ServerOpts {
         let (session_tx, session_rx) = chan();
         let watcher = Arc::new(FilesWatcher::new(&root_path, session_rx));
 
-        self.spawn_opener();
+        let root_url = format!("http://[{}]:{}/index.html", &self.addr, &self.port);
+        std::thread::spawn(move || {
+            opener::open(&root_url).unwrap();
+        });
 
         HttpServer::new(move || {
             let session_tx = session_tx.clone();
@@ -54,14 +60,8 @@ impl ServerOpts {
                 .service(web::resource("/ch-ch-ch-changes").route(web::get().to(
                     move |req, stream: web::Payload| {
                         let session_tx = session_tx.clone();
-                        ws::start(
-                            ChangeWatchSession {
-                                last_heartbeat: Instant::now(),
-                                session_tx,
-                            },
-                            &req,
-                            stream,
-                        )
+                        let session = ChangeWatchSession::new(session_tx);
+                        ws::start(session, &req, stream)
                     },
                 )))
                 .wrap(watcher_middleware)
@@ -72,13 +72,6 @@ impl ServerOpts {
         .run()
         .unwrap();
         Ok(())
-    }
-
-    fn spawn_opener(&self) {
-        let root_url = format!("http://[{}]:{}/index.html", &self.addr, &self.port);
-        std::thread::spawn(move || {
-            opener::open(&root_url).unwrap();
-        });
     }
 }
 
@@ -132,23 +125,26 @@ fn pump_channels(
     }
 }
 
-struct Changed(String);
-
-impl Message for Changed {
-    type Result = ();
-}
-
 struct FilesWatcher {
     uri_tx: Sender<Uri>,
+    joiner: Option<JoinHandle<()>>,
 }
 
 impl FilesWatcher {
     fn new(root_path: &Path, session_rx: Receiver<Addr<ChangeWatchSession>>) -> Self {
         let (uri_tx, uri_rx) = chan();
         let root = root_path.to_owned();
-        std::thread::spawn(|| pump_channels(root, (uri_rx, session_rx)));
+        let joiner = Some(std::thread::spawn(|| {
+            pump_channels(root, (uri_rx, session_rx))
+        }));
 
-        Self { uri_tx }
+        Self { uri_tx, joiner }
+    }
+}
+
+impl Drop for FilesWatcher {
+    fn drop(&mut self) {
+        self.joiner.take().unwrap().join().unwrap();
     }
 }
 
@@ -200,81 +196,16 @@ where
     }
 
     fn call(&mut self, req: Self::Request) -> Self::Future {
-        // TODO inspect the request to see whether we want to add it to watch paths
-        let request_uri = req.uri().clone();
-        let uri_tx = self.uri_tx.clone();
         let result = self.service.call(req);
+        let uri_tx = self.uri_tx.clone();
         let mapped = result.map(move |response| {
-            uri_tx
-                .send(request_uri)
-                .unwrap_or_else(|_| warn!("wasn't able to send a new uri to watch"));
+            if response.status().is_success() {
+                uri_tx
+                    .send(response.request().uri().clone())
+                    .unwrap_or_else(|_| warn!("wasn't able to send a new uri to watch"));
+            }
             response
         });
         Box::new(mapped)
-    }
-}
-
-struct ChangeWatchSession {
-    last_heartbeat: Instant,
-    session_tx: Sender<Addr<ChangeWatchSession>>,
-}
-
-impl ChangeWatchSession {
-    fn tick_heartbeat(&mut self) {
-        self.last_heartbeat = Instant::now();
-    }
-}
-
-impl Handler<Changed> for ChangeWatchSession {
-    type Result = ();
-    fn handle(
-        &mut self,
-        Changed(changed): Changed,
-        cx: &mut <Self as Actor>::Context,
-    ) -> Self::Result {
-        info!("notifying client of changed file");
-        cx.text(changed);
-    }
-}
-
-impl StreamHandler<ws::Message, ws::ProtocolError> for ChangeWatchSession {
-    fn handle(&mut self, msg: ws::Message, cx: &mut Self::Context) {
-        match msg {
-            ws::Message::Ping(msg) => {
-                self.tick_heartbeat();
-                cx.pong(&msg);
-            }
-            ws::Message::Pong(_) => {
-                self.tick_heartbeat();
-            }
-            ws::Message::Close(_) => {
-                cx.stop();
-            }
-            ws::Message::Nop => (),
-            ws::Message::Text(text) => {
-                self.tick_heartbeat();
-                debug!("ignoring text ws message {:?}", text);
-            }
-            ws::Message::Binary(_bin) => {
-                self.tick_heartbeat();
-                debug!("ignoring binary ws message");
-            }
-        }
-    }
-}
-
-impl Actor for ChangeWatchSession {
-    type Context = ws::WebsocketContext<Self>;
-
-    fn started(&mut self, cx: &mut Self::Context) {
-        self.session_tx.send(cx.address()).unwrap();
-        cx.run_interval(Duration::from_secs(3), |session, cx| {
-            if Instant::now().duration_since(session.last_heartbeat) > Duration::from_secs(10) {
-                info!("ws change event client timed out, disconnecting");
-                cx.stop();
-                return;
-            }
-            cx.ping("");
-        });
     }
 }
