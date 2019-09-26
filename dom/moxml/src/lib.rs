bd73b39149a4cbc2aa79330467d01caf67848560
Adam Perry <adam.n.perry@gmail.com>
2019-09-25 22:29:26 -0700

    [moxml] error handling and reporting

--- dom/moxml/src/lib.rs
+++ dom/moxml/src/lib.rs
@@ -1,12 +1,42 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+use {
+    proc_macro::TokenStream,
+    proc_macro_error::{filter_macro_errors, MacroError, ResultExt},
+    snax::{ParseError, SnaxItem},
+};
 
 #[proc_macro_hack::proc_macro_hack]
 pub fn moxml(input: TokenStream) -> TokenStream {
-    let item = snax::parse(input.into()).unwrap();
+    filter_macro_errors! {
+        let item = snax::parse(input.into()).map_err(Error::SnaxError).unwrap_or_exit();
+        declare_elements(item).unwrap_or_exit()
+    }
+}
 
+fn declare_elements(item: SnaxItem) -> Result<TokenStream, Error> {
     unimplemented!()
+}
+
+enum Error {
+    SnaxError(ParseError),
+}
+
+impl Into<MacroError> for Error {
+    fn into(self) -> MacroError {
+        match self {
+            Error::SnaxError(ParseError::UnexpectedEnd) => {
+                MacroError::call_site(format!("input ends before expected"))
+            }
+            Error::SnaxError(ParseError::UnexpectedItem(item)) => {
+                // TODO https://github.com/LPGhatguy/snax/issues/9
+                MacroError::call_site(format!("did not expect {:?}", item))
+            }
+            Error::SnaxError(ParseError::UnexpectedToken(token)) => {
+                MacroError::new(token.span(), format!("did not expect '{}'", token))
+            }
+        }
+    }
 }
 
 #[cfg(test)]
