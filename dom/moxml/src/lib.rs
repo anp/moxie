f6c9b72849623c432e0afeb7fafbe78d92a6b923
Adam Perry <adam.n.perry@gmail.com>
2019-09-25 23:09:29 -0700

    [moxml] expand more of the structural elements
    tags, self closing tags, etc that don't really produce code themselves

--- dom/moxml/src/lib.rs
+++ dom/moxml/src/lib.rs
@@ -3,6 +3,7 @@ extern crate proc_macro;
 use {
     proc_macro2::{Ident, TokenStream, TokenTree},
     proc_macro_error::{filter_macro_errors, MacroError, ResultExt},
+    quote::quote,
     snax::{ParseError, SnaxAttribute, SnaxFragment, SnaxItem, SnaxSelfClosingTag, SnaxTag},
 };
 
@@ -24,6 +25,19 @@ fn expand_item(item: SnaxItem) -> Result<TokenStream, Error> {
     Ok(output.into())
 }
 
+fn expand_element(name: Ident) -> TokenStream {
+    quote!(moxie_dom::element!(stringify!(#name)))
+}
+
+fn expand_attribute(attr: SnaxAttribute) -> Result<TokenStream, Error> {
+    panic!("expand_attribute")
+}
+
+fn expand_content(content: TokenTree) -> Result<TokenStream, Error> {
+    // TODO detect whether this is a formatting block or not?
+    panic!("expand_content")
+}
+
 fn expand_tag(
     SnaxTag {
         name,
@@ -32,35 +46,34 @@ fn expand_tag(
     }: SnaxTag,
 ) -> Result<TokenStream, Error> {
     Ok(cons(
-        cons(expand_element(name)?, expand_attributes(attributes)?),
+        cons(expand_element(name), expand_attributes(attributes)?),
         expand_children(children)?,
     ))
 }
 
-fn expand_self_closing_tag(tag: SnaxSelfClosingTag) -> Result<TokenStream, Error> {
-    panic!("expand_self_closing_tag")
-}
-
-fn expand_element(name: Ident) -> Result<TokenStream, Error> {
-    panic!("expand_element")
+fn expand_self_closing_tag(
+    SnaxSelfClosingTag { name, attributes }: SnaxSelfClosingTag,
+) -> Result<TokenStream, Error> {
+    Ok(cons(expand_element(name), expand_attributes(attributes)?))
 }
 
 fn expand_attributes(attrs: Vec<SnaxAttribute>) -> Result<TokenStream, Error> {
-    panic!("expand_attributes")
+    cons_fallible_iter(attrs, expand_attribute)
 }
 
 fn expand_children(children: Vec<SnaxItem>) -> Result<TokenStream, Error> {
-    Ok(children
-        .into_iter()
-        .map(expand_item)
-        .collect::<Result<Vec<_>, _>>()?
-        .into_iter()
-        .fold(quote::quote!(), cons))
+    cons_fallible_iter(children, expand_item)
 }
 
-fn expand_content(content: TokenTree) -> Result<TokenStream, Error> {
-    // TODO detect whether this is a formatting block or not?
-    panic!("expand_content")
+fn cons_fallible_iter<T>(
+    i: impl IntoIterator<Item = T>,
+    op: impl FnMut(T) -> Result<TokenStream, Error>,
+) -> Result<TokenStream, Error> {
+    Ok(i.into_iter()
+        .map(op)
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .fold(quote!(), cons))
 }
 
 fn cons(mut l: TokenStream, r: TokenStream) -> TokenStream {
