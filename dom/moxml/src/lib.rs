a36ef3001c5d9d00ca03d36e9ff8367d02533066
Adam Perry <adam.n.perry@gmail.com>
2019-09-25 21:22:20 -0700

    mox -> moxml

--- dom/moxml/src/lib.rs
+++ dom/moxml/src/lib.rs
@@ -3,8 +3,8 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 #[proc_macro]
-pub fn mox(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn moxml(input: TokenStream) -> TokenStream {
+    let item = snax::parse(input.into()).unwrap();
 
     unimplemented!()
 }
