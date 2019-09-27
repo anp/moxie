76b420f44737283e8395692315c87116354b82f1
Adam Perry <adam.n.perry@gmail.com>
2019-09-26 18:53:23 -0700

    [moxml] create our own structs instead of trying to do it all in codegen

--- dom/moxml/src/lib.rs
+++ dom/moxml/src/lib.rs
@@ -1,111 +1,131 @@
 extern crate proc_macro;
 
 use {
-    proc_macro2::{Ident, TokenStream, TokenTree},
+    proc_macro2::{Group, Ident, TokenStream, TokenTree},
     proc_macro_error::{filter_macro_errors, MacroError, ResultExt},
     quote::{quote, ToTokens},
     snax::{ParseError, SnaxAttribute, SnaxFragment, SnaxItem, SnaxSelfClosingTag, SnaxTag},
-    syn::parse::{Parse, ParseStream, Result as ParseResult},
+    std::convert::TryFrom,
 };
 
 #[proc_macro_hack::proc_macro_hack]
 pub fn moxml(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     filter_macro_errors! {
         let item = snax::parse(input.into()).map_err(Error::SnaxError).unwrap_or_exit();
-        expand_item(item).unwrap_or_exit().into()
+        let item = MoxItem::try_from(item).unwrap_or_exit();
+        quote!(#item).into()
     }
 }
 
-fn expand_item(item: SnaxItem) -> Result<TokenStream, Error> {
-    let output = match item {
-        SnaxItem::Tag(tag) => expand_tag(tag),
-        SnaxItem::SelfClosingTag(tag) => expand_self_closing_tag(tag),
-        SnaxItem::Fragment(SnaxFragment { children }) => expand_children(children),
-        SnaxItem::Content(atom) => expand_content(atom),
-    }?;
-    Ok(output.into())
+enum MoxItem {
+    Tag(MoxTag),
+    Fragment(Vec<MoxItem>),
+    Content(Content),
 }
 
-fn expand_element(name: Ident) -> TokenStream {
-    // TODO call a custom component function instead of making an element with a name
-    quote!(moxie_dom::element!(stringify!(#name)))
+impl TryFrom<SnaxItem> for MoxItem {
+    type Error = Error;
+
+    fn try_from(item: SnaxItem) -> Result<Self, Self::Error> {
+        Ok(match item {
+            SnaxItem::Tag(t) => MoxItem::Tag(MoxTag::try_from(t)?),
+            SnaxItem::SelfClosingTag(t) => MoxItem::Tag(MoxTag::try_from(t)?),
+            SnaxItem::Fragment(SnaxFragment { children }) => MoxItem::Fragment(
+                children
+                    .into_iter()
+                    .map(MoxItem::try_from)
+                    .collect::<Result<_, _>>()?,
+            ),
+            SnaxItem::Content(atom) => MoxItem::Content(Content::try_from(atom)?),
+        })
+    }
 }
 
-fn expand_attribute(attr: SnaxAttribute) -> Result<TokenStream, Error> {
-    // TODO call .attr unless the name starts with `on`, then do event dispatch
-    Ok(quote!())
+impl ToTokens for MoxItem {
+    fn to_tokens(&self, tokens: &mut TokenStream) {}
 }
 
-fn expand_content(content: TokenTree) -> Result<TokenStream, Error> {
-    Ok(match content {
-        TokenTree::Ident(i) => quote!(#i),
-        TokenTree::Punct(p) => quote!(#p),
-        TokenTree::Literal(l) => quote!(#l),
-        TokenTree::Group(g) => {
-            let braced: BracedExpr = syn::parse2(g.stream()).map_err(Error::SynError)?;
-            quote!(#braced)
+struct MoxTag {
+    name: Ident,
+    attributes: Vec<MoxAttr>,
+    children: Vec<MoxItem>,
+}
+
+impl TryFrom<SnaxTag> for MoxTag {
+    type Error = Error;
+    fn try_from(
+        SnaxTag {
+            name,
+            attributes,
+            children,
+        }: SnaxTag,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            name,
+            attributes: attributes.into_iter().map(MoxAttr::from).collect(),
+            children: children
+                .into_iter()
+                .map(MoxItem::try_from)
+                .collect::<Result<_, _>>()?,
+        })
+    }
+}
+
+impl TryFrom<SnaxSelfClosingTag> for MoxTag {
+    type Error = Error;
+    fn try_from(
+        SnaxSelfClosingTag { name, attributes }: SnaxSelfClosingTag,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            name,
+            attributes: attributes.into_iter().map(MoxAttr::from).collect(),
+            children: vec![],
+        })
+    }
+}
+
+enum MoxAttr {
+    Simple { name: Ident, value: TokenTree },
+    Handler { name: Ident, value: TokenTree },
+}
+
+impl From<SnaxAttribute> for MoxAttr {
+    fn from(attr: SnaxAttribute) -> Self {
+        match attr {
+            SnaxAttribute::Simple { name, value } => MoxAttr::Simple { name, value },
         }
-    })
-}
-
-enum BracedExpr {
-    Format(TokenTree),
-    Expr(TokenTree),
-}
-
-impl Parse for BracedExpr {
-    fn parse(input: ParseStream) -> ParseResult<Self> {
-        unimplemented!("parsing")
     }
 }
 
-impl ToTokens for BracedExpr {
-    fn to_tokens(&self, tokens: &mut TokenStream) {
-        unimplemented!("expansion")
+enum Content {
+    FormatExpr(TokenTree),
+    RustExpr(TokenTree),
+}
+
+impl TryFrom<TokenTree> for Content {
+    type Error = Error;
+    fn try_from(tt: TokenTree) -> Result<Self, Self::Error> {
+        Ok(match tt {
+            tt @ TokenTree::Ident(_) | tt @ TokenTree::Literal(_) | tt @ TokenTree::Punct(_) => {
+                Content::RustExpr(tt)
+            }
+            TokenTree::Group(mut g) => {
+                let mut tokens = g.stream().into_iter().peekable();
+                if let Some(TokenTree::Punct(p)) = tokens.next() {
+                    if p.as_char() == '%' {
+                        let mut new_stream = quote!();
+                        // TODO get all but the last element here too if its a %
+                        new_stream.extend(tokens);
+                        Content::FormatExpr(TokenTree::Group(Group::new(g.delimiter(), new_stream)))
+                    } else {
+                        Content::RustExpr(g.into())
+                    }
+                } else {
+                    Content::RustExpr(g.into())
+                }
+            }
+        })
     }
-}
-
-fn expand_tag(
-    SnaxTag {
-        name,
-        attributes,
-        children,
-    }: SnaxTag,
-) -> Result<TokenStream, Error> {
-    Ok(cons(
-        cons(expand_element(name), expand_attributes(attributes)?),
-        expand_children(children)?,
-    ))
-}
-
-fn expand_self_closing_tag(
-    SnaxSelfClosingTag { name, attributes }: SnaxSelfClosingTag,
-) -> Result<TokenStream, Error> {
-    Ok(cons(expand_element(name), expand_attributes(attributes)?))
-}
-
-fn expand_attributes(attrs: Vec<SnaxAttribute>) -> Result<TokenStream, Error> {
-    cons_fallible_iter(attrs, expand_attribute)
-}
-
-fn expand_children(children: Vec<SnaxItem>) -> Result<TokenStream, Error> {
-    cons_fallible_iter(children, expand_item)
-}
-
-fn cons_fallible_iter<T>(
-    i: impl IntoIterator<Item = T>,
-    op: impl FnMut(T) -> Result<TokenStream, Error>,
-) -> Result<TokenStream, Error> {
-    Ok(i.into_iter()
-        .map(op)
-        .collect::<Result<Vec<_>, _>>()?
-        .into_iter()
-        .fold(quote!(), cons))
-}
-
-fn cons(mut l: TokenStream, r: TokenStream) -> TokenStream {
-    l.extend(r);
-    l
 }
 
 enum Error {
