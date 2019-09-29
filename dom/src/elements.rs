cfddc62d6d93c182be8918b803d0e9218d2f3aa0
Adam Perry <adam.n.perry@gmail.com>
2019-09-29 13:49:40 -0700

    [dom] form elem doc cleanup

--- dom/src/elements.rs
+++ dom/src/elements.rs
@@ -871,98 +871,123 @@ element_macro! {
 }
 
 element_macro! {
-    /// The HTML `<button>` element represents a clickable button, which can be used in <a href="https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms">forms</a> or anywhere in a document that needs simple, standard button functionality.
+    /// The [HTML `<button>` element][mdn] represents a clickable button, which can be used in
+    /// [forms] or anywhere in a document that needs simple, standard button functionality.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button
+    /// [forms]: https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms
     button
 }
 
 element_macro! {
-    /// The HTML `<datalist>` element contains a set of <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/option" title="The HTML <option> element is used to define an item contained in a <select>, an <optgroup>, or a <datalist>&nbsp;element. As such,&nbsp;<option>&nbsp;can represent menu items in popups and other lists of items in an HTML document.">`<option>`</a> elements that represent the values available for other controls.
+    /// The [HTML `<datalist>` element][mdn] contains a set of [`<option>`][option] elements that
+    /// represent the values available for other controls.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist
+    /// [option]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/option
     datalist
 }
 
 element_macro! {
-    /// The HTML `<fieldset>` element is used to group several controls as well as labels (<a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/label" title="The HTML <label> element represents a caption for an item in a user interface.">`<label>`</a>) within a web form.
+    /// The [HTML `<fieldset>` element][mdn] is used to group several controls as well as labels
+    /// ([`<label>`][label]) within a web form.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/fieldset
+    /// [label]: <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/label" title="The HTML <label> element represents a caption for an item in a user interface.">
     fieldset
 }
 
 element_macro! {
-    /// The HTML `<form>` element represents a document section that contains interactive controls for submitting information to a web server.
+    /// The [HTML `<form>` element][mdn] represents a document section that contains interactive
+    /// controls for submitting information to a web server.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/form
     form
 }
 
 element_macro! {
-    /// The HTML `<input>` element is used to create interactive controls for web-based forms in order to accept data from the user; a wide variety of types of input data and control widgets are available, depending on the device and <a class="glossaryLink" href="https://developer.mozilla.org/en-US/docs/Glossary/user_agent" title="user agent: A user agent is a computer program representing a person, for example, a browser in a Web context.">user agent</a>.
+    /// The [HTML `<input>` element][mdn] is used to create interactive controls for web-based forms
+    /// in order to accept data from the user; a wide variety of types of input data and control
+    /// widgets are available, depending on the device and [user agent].
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input
+    /// [user agent]: https://developer.mozilla.org/en-US/docs/Glossary/user_agent
     input
 }
 
 element_macro! {
-    /// The HTML `<label>` element represents a caption for an item in a user interface.
+    /// The [HTML `<label>` element][mdn] represents a caption for an item in a user interface.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/label
     label
 }
 
 element_macro! {
-    /// The HTML `<legend>` element represents a caption for the content of its parent <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/fieldset" title="The HTML <fieldset> element is used to group several controls as well as labels (<label>) within a web form.">`<fieldset>`</a>.
+    /// The [HTML `<legend>` element][mdn] represents a caption for the content of its parent
+    /// [`<fieldset>`][fieldset].
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/legend
+    /// [fieldset]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/fieldset
     legend
 }
 
 element_macro! {
-    /// The HTML `<meter>` element represents either a scalar value within a known range or a fractional value.
+    /// The [HTML `<meter>` element][mdn] represents either a scalar value within a known range or a
+    /// fractional value.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meter
     meter
 }
 
 element_macro! {
-    /// The HTML `<optgroup>` element creates a grouping of options within a <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select" title="The HTML <select> element represents a control that provides a menu of options">`<select>`</a> element.
+    /// The [HTML `<optgroup>` element][mdn] creates a grouping of options within a
+    /// [`<select>`][select] element.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/optgroup
+    /// [select]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
     optgroup
 }
 
 element_macro! {
-    /// The HTML `<option>` element is used to define an item contained in a <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select" title="The HTML <select> element represents a control that provides a menu of options">`<select>`</a>, an <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/optgroup" title="The HTML <optgroup> element creates a grouping of options within a <select> element.">`<optgroup>`</a>, or a <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist" title="The HTML <datalist> element contains a set of <option> elements that represent the values available for other controls.">`<datalist>`</a>&nbsp;element. As such,&nbsp;`<option>`&nbsp;can represent menu items in popups and other lists of items in an HTML document.
+    /// The [HTML `<option>` element][mdn] is used to define an item contained in a
+    /// [`<select>`][select], an [`<optgroup>`][optgroup], or a [`<datalist>`][datalist] element. As
+    /// such, `<option>` can represent menu items in popups and other lists of items in an HTML
+    /// document.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/option
+    /// [select]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
+    /// [optgroup]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/optgroup
+    /// [datalist]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist
     option
 }
 
 element_macro! {
-    /// The HTML Output element (`<output>`) is a container element into which a site or app can inject the results of a calculation or the outcome of a user action.
+    /// The [HTML Output element (`<output>`)][mdn] is a container element into which a site or app
+    /// can inject the results of a calculation or the outcome of a user action.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/output
     output
 }
 
 element_macro! {
-    /// The HTML `<progress>` element displays an indicator showing the completion progress of a task, typically displayed as a progress bar.
+    /// The [HTML `<progress>` element][progress] displays an indicator showing the completion
+    /// progress of a task, typically displayed as a progress bar.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/progress
     progress
 }
 
 element_macro! {
-    /// The HTML `<select>` element represents a control that provides a menu of options
+    /// The [HTML `<select>` element][mdn] represents a control that provides a menu of options.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
     select
 }
 
 element_macro! {
-    /// The HTML `<textarea>` element represents a multi-line plain-text editing control, useful when you want to allow users to enter a sizeable amount of free-form text, for example a comment on a review or feedback form.
+    /// The [HTML `<textarea>` element][mdn] represents a multi-line plain-text editing control,
+    /// useful when you want to allow users to enter a sizeable amount of free-form text, for
+    /// example a comment on a review or feedback form.
     ///
     /// [mdn]: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/textarea
     textarea
