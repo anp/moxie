20fb613eac4768cf636bf60f769e02e7691e847b
Adam Perry <adam.n.perry@gmail.com>
2019-09-29 22:58:09 -0700

    [dom] add docs and remove warnings

--- dom/src/lib.rs
+++ dom/src/lib.rs
@@ -1,4 +1,7 @@
-#![warn(missing_docs)]
+//! Tools for declaratively constructing and incrementally updating HTML DOM trees on the web. Based
+//! on the [`moxie`] UI runtime.
+
+#![warn(missing_docs, intra_doc_link_resolution_failure)]
 
 #[doc(hidden)]
 pub use moxie::*;
@@ -48,6 +51,8 @@ pub fn document() -> sys::Document {
         .expect("must run from within a `window` with a valid `document`")
 }
 
+/// Create and mount a [DOM text node](https://developer.mozilla.org/en-US/docs/Web/API/Text).
+/// This is normally called by the [`moxie::mox!`] macro.
 #[topo::aware]
 #[topo::from_env(parent: MemoElement)]
 pub fn text(s: impl ToString) {
@@ -58,15 +63,32 @@ pub fn text(s: impl ToString) {
     parent.ensure_child_attached(&text_node);
 }
 
+/// Create and mount an [HTML element](https://developer.mozilla.org/en-US/docs/Web/API/Element).
+/// Called from the individual element macros, which in turn are normally called by the output of
+/// the [`moxie::mox!`] macro.
+///
+/// The created DOM node is memoized at the bound callsite, allowing for subsequent re-executions to
+/// be very cheap.
+///
+/// Mutation of the created element is performed during the `with_elem` closure via the provided
+/// [`moxie_dom::MemoElement`] wrapper.
 #[topo::aware]
 #[topo::from_env(parent: MemoElement)]
-pub fn element<ChildRet>(ty: &'static str, with_elem: impl FnOnce(&MemoElement) -> ChildRet) {
+pub fn element<ChildRet>(
+    ty: &'static str,
+    with_elem: impl FnOnce(&MemoElement) -> ChildRet,
+) -> ChildRet {
     let elem = memo!(ty, |ty| document().create_element(ty).unwrap());
     parent.ensure_child_attached(&elem);
     let elem = MemoElement::new(elem);
-    with_elem(&elem);
+    with_elem(&elem)
 }
 
+/// A topologically-aware "incremental smart pointer" for an HTML element.
+///
+/// Created during execution of the (element) macro and the element-specific wrappers. Offers a
+/// "stringly-typed" API for mutating the contained DOM nodes, adhering fairly closely to the
+/// upstream web specs.
 pub struct MemoElement {
     curr: Cell<Option<sys::Node>>,
     elem: sys::Element,
@@ -80,17 +102,28 @@ impl MemoElement {
         }
     }
 
-    /// Retrieves access to the raw HTML element underlying the `MemoElement`.
+    /// Retrieves access to the raw HTML element underlying the (MemoElement).
     ///
     /// Because this offers an escape hatch around the memoized mutations, it should be used with
     /// caution. Also because of this, it has a silly name intended to loudly announce that
     /// care must be taken.
+    ///
+    /// Code called by the root function of your application will be run quite frequently and
+    /// so the tools for memoization are important for keeping your application responsive. If you
+    /// have legitimate needs for this API, please consider filing an issue with your use case so
+    /// the maintainers of this crate can consider "official" ways to support it.
     pub fn raw_element_that_has_sharp_edges_please_be_careful(&self) -> sys::Element {
         self.elem.clone()
     }
 
     // FIXME this should be topo-aware
     // TODO and it should be able to express its slot as an annotation
+    /// Declare an attribute of the element, mutating the actual element's attribute when the passed
+    /// value changes.
+    ///
+    /// A guard value is stored as a resulting "effect" of the mutation, and removes the attribute
+    /// when `drop`ped, to ensure that the attribute is removed when this declaration is no longer
+    /// referenced in the most recent (`moxie::Revision`).
     pub fn attr(&self, name: &'static str, value: impl ToString) -> &Self {
         topo::call!(slot: name, {
             memo_with!(
@@ -108,6 +141,14 @@ impl MemoElement {
     }
 
     // FIXME this should be topo-aware
+    /// Declare an event handler on the element.
+    ///
+    /// A guard value is stored as a resulting "effect" of the mutation, and removes the attribute
+    /// when `drop`ped, to ensure that the attribute is removed when this declaration is no longer
+    /// referenced in the most recent (`moxie::Revision`).
+    ///
+    /// Currently this is performed on every Revision, as changes to event handlers don't typically
+    /// affect the debugging experience and have not yet shown up in performance profiles.
     pub fn on<Ev>(&self, callback: impl FnMut(Ev) + 'static) -> &Self
     where
         Ev: 'static + Event,
@@ -143,6 +184,9 @@ impl MemoElement {
         }
     }
 
+    /// Declare the inner contents of the element, usually declaring children within the inner
+    /// scope. After any children have been run and their nodes attached, this clears any trailing
+    /// child nodes to ensure the element's children are correct per the latest declaration.
     // FIXME this should be topo-aware
     pub fn inner<Ret>(&self, children: impl FnOnce() -> Ret) -> Ret {
         let elem = self.elem.clone();
@@ -161,6 +205,8 @@ impl MemoElement {
             }
         );
 
+        // if there weren't any children declared this revision, we need to make sure we clean up
+        // any from the last revision
         let mut next_to_remove = if let Some(c) = last_desired_child {
             c.next_sibling()
         } else {
