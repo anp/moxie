2b4066a44b15e51fbe1e69363ac1fc394b871d69
Adam Perry <adam.n.perry@gmail.com>
2019-10-06 22:38:58 -0700

    [ssr] add barebones proof-of-concept SSR test with gotham

--- dom/examples/ssr/src/main.rs
+++ dom/examples/ssr/src/main.rs
@@ -1,10 +1,102 @@
+#[macro_use]
+extern crate gotham_derive;
+#[macro_use]
+extern crate serde_derive;
+
+use {
+    gotham::router::builder::*,
+    gotham::router::Router,
+    gotham::state::{FromState, State},
+    moxie_dom::*,
+};
+
 fn main() {
-    println!("hi");
+    let addr = "127.0.0.1:7878";
+    println!("Listening for requests at http://{}", addr);
+    gotham::start(addr, router())
+}
+
+#[derive(Deserialize, StateData, StaticResponseExtender)]
+struct PathExtractor {
+    #[serde(rename = "*")]
+    parts: Vec<String>,
+}
+
+#[topo::aware]
+fn simple_list(items: &[String]) {
+    moxie::mox! {
+        <ul>{
+            for item in items {
+                moxie::mox!(<li>{% "{}", item }</li>)
+            }
+        }</ul>
+    }
+}
+
+fn parts_handler(state: State) -> (State, String) {
+    let parts = {
+        let path = PathExtractor::borrow_from(&state);
+        path.parts.to_owned()
+    };
+    let res = moxie_dom::render_html(move || simple_list!(&parts));
+    (state, res)
+}
+
+fn router() -> Router {
+    build_simple_router(|route| {
+        route
+            .get("/parts/*")
+            .with_path_extractor::<PathExtractor>()
+            .to(parts_handler);
+    })
 }
 
 #[cfg(test)]
 mod tests {
-    use moxie_dom::{embed::WebRuntime, *};
+    use {super::*, gotham::test::TestServer, hyper::StatusCode, moxie_dom::embed::WebRuntime};
+
+    #[test]
+    fn extracts_one_component() {
+        let test_server = TestServer::new(router()).unwrap();
+        let response = test_server
+            .client()
+            .get("http://localhost/parts/head")
+            .perform()
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = String::from_utf8(response.read_body().unwrap()).unwrap();
+        assert_eq!(
+            &body,
+            "<ul>
+  <li>head</li>
+</ul>",
+        );
+    }
+
+    #[test]
+    fn extracts_multiple_components() {
+        let test_server = TestServer::new(router()).unwrap();
+        let response = test_server
+            .client()
+            .get("http://localhost/parts/head/shoulders/knees/toes")
+            .perform()
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = String::from_utf8(response.read_body().unwrap()).unwrap();
+        assert_eq!(
+            &body,
+            &"<ul>
+  <li>head</li>
+  <li>shoulders</li>
+  <li>knees</li>
+  <li>toes</li>
+</ul>",
+        );
+    }
 
     #[test]
     fn basic_list_prerender() {
