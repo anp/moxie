d3befec03fa8e21d39e7b52fcea2cd849b8e6f74
Adam Perry <adam.n.perry@gmail.com>
2019-11-17 15:26:02 -0800

    [illicit] Print the environment as a stack of additions.

--- illicit/src/anon_rc.rs
+++ illicit/src/anon_rc.rs
@@ -8,38 +8,31 @@ use {
 };
 
 #[doc(hidden)]
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub(crate) struct AnonRc {
     name: &'static str,
     id: TypeId,
+    location: (&'static str, u32, u32),
+    depth: u32,
     inner: Rc<dyn Any>,
     debug: Rc<dyn Debug>,
 }
 
 impl AnonRc {
-    /// The `TypeId` of the contained value.
-    pub fn id(&self) -> TypeId {
-        self.id
-    }
-
-    /// The typename of the contained value.
-    pub fn ty(&self) -> &str {
-        self.name
-    }
-
-    /// Returns a debuggable representation of the contained value.
-    pub fn debug(&self) -> &dyn std::fmt::Debug {
-        &*self.debug
-    }
-
     /// Construct a new `AnonArc` from the provided value.
-    pub fn new<T: Debug + 'static>(inner: T) -> Self {
+    pub fn new<T: Debug + 'static>(
+        inner: T,
+        location: (&'static str, u32, u32),
+        depth: u32,
+    ) -> Self {
         let inner = Rc::new(inner);
         Self {
             name: std::any::type_name::<T>(),
             id: TypeId::of::<T>(),
             debug: inner.clone(),
+            location,
             inner,
+            depth,
         }
     }
 
@@ -50,5 +43,30 @@ impl AnonRc {
                 panic!("asked {:?} to cast to {:?}", anon, TypeId::of::<T>(),);
             })
         })
+    }
+
+    /// The `TypeId` of the contained value.
+    pub fn id(&self) -> TypeId {
+        self.id
+    }
+
+    /// The typename of the contained value.
+    pub fn ty(&self) -> &str {
+        self.name
+    }
+
+    /// Returns a debug-printable reference to the contained value.
+    pub fn debug(&self) -> &dyn Debug {
+        &*self.debug
+    }
+
+    /// The depth of the environment where this was created.
+    pub fn depth(&self) -> u32 {
+        self.depth
+    }
+
+    /// The source location at which this was initialized for an environment.
+    pub fn location(&self) -> (&'static str, u32, u32) {
+        self.location
     }
 }
