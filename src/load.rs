33f5038ebd25de479cea171b39d321e636e3ad6c
Adam Perry <adam.n.perry@gmail.com>
2019-11-18 20:15:31 -0800

    Add tests for the executor.
    Because the executor is adapted from internals of futures::...::LocalPool, it makes sense to adapt the tests for it too. Conveniently that crate's license is the same as moxie's.

--- src/load.rs
+++ src/load.rs
@@ -7,7 +7,6 @@ use {
 };
 
 /// TODO
-#[allow(clippy::drop_ref)]
 #[topo::nested]
 #[illicit::from_env(spawner: &Spawner)]
 pub fn load_with<Arg, Fut, Stored, Ret>(
@@ -17,7 +16,7 @@ pub fn load_with<Arg, Fut, Stored, Ret>(
 ) -> Poll<Ret>
 where
     Arg: PartialEq + 'static,
-    Fut: Future<Output = Stored> + Send + 'static,
+    Fut: Future<Output = Stored> + 'static,
     Stored: 'static,
     Ret: 'static,
 {
@@ -57,7 +56,7 @@ pub fn load_once_with<Fut, Stored, Ret>(
     with: impl FnOnce(&Stored) -> Ret,
 ) -> Poll<Ret>
 where
-    Fut: Future<Output = Stored> + Send + 'static,
+    Fut: Future<Output = Stored> + 'static,
     Stored: 'static,
     Ret: 'static,
 {
@@ -68,7 +67,7 @@ where
 #[topo::nested]
 pub fn load_once<Fut, Stored>(init: impl FnOnce() -> Fut) -> Poll<Stored>
 where
-    Fut: Future<Output = Stored> + Send + 'static,
+    Fut: Future<Output = Stored> + 'static,
     Stored: Clone + 'static,
 {
     load_with!((), |()| init(), Clone::clone)
@@ -79,7 +78,7 @@ where
 pub fn load<Arg, Fut, Stored>(arg: Arg, init: impl FnOnce(&Arg) -> Fut) -> Poll<Stored>
 where
     Arg: PartialEq + 'static,
-    Fut: Future<Output = Stored> + Send + 'static,
+    Fut: Future<Output = Stored> + 'static,
     Stored: Clone + 'static,
 {
     load_with!(arg, init, Clone::clone)
