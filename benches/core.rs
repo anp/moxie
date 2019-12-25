748b1279683b69c1c9fd5334f89e323319ca11a4
Adam Perry <adam.n.perry@gmail.com>
2019-12-24 17:34:55 -0800

    fix benchmarks

--- benches/core.rs
+++ benches/core.rs
@@ -4,7 +4,7 @@ extern crate criterion;
 use criterion::{Criterion, ParameterizedBenchmark};
 use moxie::{
     embed::{Revision, Runtime},
-    *,
+    prelude::*,
 };
 use std::rc::Rc;
 
@@ -12,7 +12,7 @@ criterion::criterion_group!(runtime, once_from_store, run_empty, run_repeated);
 criterion::criterion_main!(runtime);
 
 fn once_from_store(c: &mut Criterion) {
-    let mut rt = Runtime::new(|| once!(|| Rc::new(vec![0; 1_000_000])));
+    let mut rt = Runtime::new(|| once(|| Rc::new(vec![0; 1_000_000])));
     rt.run_once();
     c.bench_function("1mb vec cached", |b| b.iter(|| rt.run_once()));
 }
