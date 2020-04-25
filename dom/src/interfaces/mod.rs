726f2b5a5b92b61070280593262bfaedb9dba4af
Adam Perry <lol@anp.lol>
2020-04-25 13:44:14 -0700

    [dom] Implement boolean attributes correctly.
    Fixes #61.

--- dom/src/interfaces/mod.rs
+++ dom/src/interfaces/mod.rs
@@ -14,6 +14,22 @@ macro_rules! attr_method {
     };
 }
 
+macro_rules! bool_attr_method {
+    (
+        $(#[$outer:meta])*
+        $attr:ident
+    ) => {
+        $(#[$outer])*
+        #[topo::nested]
+        fn $attr(&self, to_set: bool) -> &Self {
+            if to_set {
+                self.attribute(stringify!($attr), "".to_string());
+            }
+            self
+        }
+    };
+}
+
 pub mod element;
 pub mod event_target;
 pub mod html_element;
