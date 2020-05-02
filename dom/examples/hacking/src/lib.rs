fc785f64138ff8e569d1fa425238808574ed36a3
Adam Perry <lol@anp.lol>
2020-05-02 09:46:26 -0700

    [dom][hacking] Start a basic test.

--- dom/examples/hacking/src/lib.rs
+++ dom/examples/hacking/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(track_caller)]
+
 use moxie_dom::{
     elements::{forms::button, text_content::div},
     prelude::*,
@@ -12,30 +14,42 @@ pub fn begin() {
     }));
 
     tracing::info!("mounting moxie-dom to root");
-    moxie_dom::boot(document().body().unwrap(), || {
-        let count = state(|| 0);
+    moxie_dom::boot(document().body().unwrap(), root);
+}
 
-        mox! {<>
-            <div>{% "hello world from moxie! ({})", &count }</div>
+#[topo::nested]
+fn root() {
+    let count = state(|| 0);
 
-            <button type="button" onclick={move |_| count.update(|c| Some(c + 1))}>
-                "increment"
-            </button>
-        </>};
+    mox! {<>
+        <div>{% "hello world from moxie! ({})", &count }</div>
 
-        for t in &["first", "second", "third"] {
-            mox! { <div>{% "{}", t }</div> };
-        }
-    });
+        <button type="button" onclick={move |_| count.update(|c| Some(c + 1))}>
+            "increment"
+        </button>
+    </>};
+
+    for t in &["first", "second", "third"] {
+        mox! { <div>{% "{}", t }</div> };
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use wasm_bindgen_test::*;
     wasm_bindgen_test_configure!(run_in_browser);
 
     #[wasm_bindgen_test]
     pub fn hello_browser() {
-        println!("hello");
+        let test_root = augdom::Node::new("div");
+        moxie_dom::boot(test_root, root);
+
+        // TODO wait until first animation frame callback done
+        // TODO assert HTML matches expected
+        // TODO find button
+        // TODO click button
+        // TODO assert HTML matches expected, all nodes identical
+        // TODO install mutation observer, enumerate exact mutations
     }
 }
