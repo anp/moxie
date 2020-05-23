6cfae55aadf8d5fd40ddc4e0ca1a46eb9178fc4e
Adam Perry <lol@anp.lol>
2020-05-23 13:47:59 -0700

    [dom] Remove typed-html example.
    Can't rustup with typed-html, something in my usage causes a type overflow.

--- dom/examples/drivertest/src/lib.rs
+++ dom/examples/drivertest/src/lib.rs
@@ -3,8 +3,6 @@ use moxie_dom::{
     embed::WebRuntime,
     prelude::*,
 };
-use std::io::prelude::*;
-use typed_html::dom::{DOMTree, VNode};
 use wasm_bindgen::JsCast;
 use wasm_bindgen_test::*;
 use web_sys as sys;
@@ -12,17 +10,6 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn mini_list() {
-    let mut expected: DOMTree<String> = typed_html::html!(
-        <div>
-            <ul class="listywisty">
-                <li>"first"</li>
-                <li class="item">"second"</li>
-                <li>"third"</li>
-            </ul>
-        </div>
-    );
-    let expected = expected.vnode();
-
     let list = || {
         moxie::mox! {
             <ul class="listywisty">
@@ -39,8 +26,6 @@ fn mini_list() {
 
     web_tester.run_once();
     virtual_tester.run_once();
-
-    assert_vnode_matches_element(&expected, &web_div);
 
     let expected_html = r#"<div><ul class="listywisty"><li>first</li><li class="item">second</li><li>third</li></ul></div>"#;
 
@@ -131,69 +116,4 @@ fn mutiple_event_listeners() {
         "counter1 = 1, counter2 = 1",
         "Counters should be updated once"
     );
-}
-
-fn assert_vnode_matches_element(expected: &VNode<String>, actual: &sys::Node) {
-    match (expected, actual.node_type()) {
-        (VNode::Text(expected), sys::Node::TEXT_NODE) => {
-            assert_eq!(*expected, &actual.text_content().unwrap());
-        }
-        (VNode::Element(expected), sys::Node::ELEMENT_NODE) => {
-            let actual: &sys::Element = actual.dyn_ref().unwrap();
-            assert_eq!(
-                expected.name.to_lowercase(),
-                actual.node_name().to_lowercase(),
-                "element types must match",
-            );
-
-            assert_attributes_match(expected, actual);
-
-            let mut actual_child = actual.first_child();
-            for (i, expected_child) in expected.children.iter().enumerate() {
-                let child = match actual_child {
-                    Some(a) => a,
-                    None => {
-                        panic!("failed while looking for child {} of {}", i, actual.inner_html())
-                    }
-                };
-                assert_vnode_matches_element(expected_child, &child);
-                actual_child = child.next_sibling();
-            }
-            assert!(actual_child.is_none(), "dom node should not have any children remaining");
-        }
-        _ => {
-            panic!("mismatched nodes!");
-        }
-    }
-}
-
-fn assert_attributes_match(expected: &typed_html::dom::VElement<String>, actual: &sys::Element) {
-    let mut attr_panic_msg = Vec::new();
-
-    let mut expected_attrs = std::collections::BTreeMap::new();
-    for (name, value) in &expected.attributes {
-        expected_attrs.insert(name.to_string(), value);
-    }
-
-    let actual_attrs = actual.attributes();
-    for i in 0..actual_attrs.length() {
-        let actual = actual_attrs.item(i).unwrap();
-        let name = actual.local_name();
-        let expected = expected_attrs.remove(&name);
-
-        if let Some(expected) = expected {
-            assert_eq!(&actual.value(), expected, "attribute `{}` must match", name);
-        } else {
-            writeln!(&mut attr_panic_msg, "unexpected {}={}", name, actual.value()).unwrap();
-        }
-    }
-
-    for (expected_name, expected_value) in expected_attrs {
-        writeln!(&mut attr_panic_msg, "missing {}={}", expected_name, expected_value).unwrap();
-    }
-
-    if !attr_panic_msg.is_empty() {
-        let msg = String::from_utf8(attr_panic_msg).unwrap();
-        panic!("attributes mismatched:\n{}", msg);
-    }
 }
