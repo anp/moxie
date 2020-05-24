ceac0a015677123ae21ab87f153f97a246e093e6
Adam Perry <lol@anp.lol>
2020-05-24 16:54:01 -0700

    [ofl] Correctly initialize tracing with a global subscriber.

--- ofl/src/main.rs
+++ ofl/src/main.rs
@@ -2,7 +2,7 @@ use failure::Error;
 use gumdrop::Options;
 use std::path::{Path, PathBuf};
 use tracing::*;
-use tracing_subscriber::{filter::EnvFilter, fmt::Subscriber};
+use tracing_subscriber::{filter::LevelFilter, fmt::Subscriber, Layer};
 
 mod format;
 mod published;
@@ -41,21 +41,17 @@ impl Default for Command {
 
 fn main() -> Result<(), Error> {
     let config = Config::parse_args_default_or_exit();
-    let level = if config.verbose { "debug" } else { "info" };
-    tracing::subscriber::with_default(
-        Subscriber::builder().with_env_filter(EnvFilter::new(level)).finish(),
-        || {
-            debug!("logging init'd");
+    let level = if config.verbose { LevelFilter::DEBUG } else { LevelFilter::INFO };
+    tracing::subscriber::set_global_default(level.with_subscriber(Subscriber::new())).unwrap();
+    info!("logging init'd");
 
-            let command = config.command.unwrap_or_default();
+    let command = config.command.unwrap_or_default();
 
-            match command {
-                Command::Fmt(opts) => opts.run(config.project_root),
-                Command::Published(opts) => opts.run(config.project_root),
-                Command::Serve(opts) => opts.run_server(config.project_root),
-                Command::Website(opts) => opts.run(config.project_root),
-                Command::Versions(opts) => opts.run(config.project_root),
-            }
-        },
-    )
+    match command {
+        Command::Fmt(opts) => opts.run(config.project_root),
+        Command::Published(opts) => opts.run(config.project_root),
+        Command::Serve(opts) => opts.run_server(config.project_root),
+        Command::Website(opts) => opts.run(config.project_root),
+        Command::Versions(opts) => opts.run(config.project_root),
+    }
 }
