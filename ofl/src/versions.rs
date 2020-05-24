147ec167fdbd1be62f480c91534b7564dce8ceb6
Adam Perry <lol@anp.lol>
2020-05-23 21:28:24 -0700

    [ofl] Apply new versions after computing them.

--- ofl/src/versions.rs
+++ ofl/src/versions.rs
@@ -4,10 +4,11 @@ use failure::Error;
 use gumdrop::Options;
 use semver::Version;
 use std::{
-    collections::BTreeMap,
-    path::{Path, PathBuf},
+    collections::{BTreeMap, BTreeSet},
+    path::PathBuf,
 };
 use toml_edit::Document;
+use tracing::*;
 
 #[derive(Debug, Options)]
 pub struct Versions {}
@@ -21,7 +22,15 @@ impl Versions {
         // TODO prompt for a subset of crates to update
 
         let mut updates = Vec::new();
-        let mut updated_manifests = BTreeMap::new();
+        let mut updated_manifests = moxie_crates
+            .iter()
+            .map(|id| {
+                let moxie_crate = workspace.get_member(&id).unwrap();
+                let manifest_contents = std::fs::read_to_string(&moxie_crate.manifest_path)?;
+                let manifest: Document = manifest_contents.parse()?;
+                Ok((moxie_crate.name.clone(), (moxie_crate, manifest)))
+            })
+            .collect::<Result<BTreeMap<_, _>, Error>>()?;
         for id in moxie_crates {
             let moxie_crate = workspace.get_member(&id).unwrap();
 
@@ -32,15 +41,11 @@ impl Versions {
             }
 
             let new_version = prompt_for_new_version(&moxie_crate)?;
-            let manifest_contents = std::fs::read_to_string(&moxie_crate.manifest_path)?;
 
             if new_version != moxie_crate.version {
                 // TODO ensure downstream updates increment versions appropriately
                 updates.push((moxie_crate, new_version, workspace.local_dependents(&id)));
             }
-
-            let manifest: Document = manifest_contents.parse()?;
-            updated_manifests.insert(id, manifest);
         }
 
         let mut pending_updates = vec![String::from("crates to update:")];
@@ -61,17 +66,85 @@ impl Versions {
 
         failure::ensure!(Confirm::new().with_prompt("proceed?").interact()?);
 
-        for (id, new_version, dependents) in updates {
-            // TODO update the version in its manifest
-            // TODO update the dep version for each depending-upon manifest
+        let mut to_write = BTreeSet::new();
+        for (package, new_version, dependents) in updates {
+            let manifest = &mut updated_manifests.get_mut(&package.name).unwrap().1;
+
+            let new_version: toml_edit::Value = new_version.to_string().into();
+
+            manifest["package"]["version"] = toml_edit::Item::Value(new_version.clone());
+            to_write.insert(package.id.clone());
+
+            for dependent in dependents {
+                let manifest = &mut updated_manifests.get_mut(&dependent.name).unwrap().1;
+
+                if update_dependency_version(manifest, &package.name, &new_version) {
+                    to_write.insert(dependent.id.clone());
+                } else {
+                    debug!({ 
+                        crate = %dependent.name, 
+                        searched_for = %package.name,
+                    }, "skipping, no dependency or version found");
+                    continue;
+                }
+            }
         }
 
-        for (id, manifest) in updated_manifests {
-            // TODO write back to disk
+        for (package, manifest) in updated_manifests.values() {
+            if !to_write.contains(&package.id) {
+                debug!({ crate = %package.name }, "skipping, no file changes");
+                continue;
+            }
+
+            info!({ crate = %package.name }, "writing updated manifest");
         }
 
         Ok(())
     }
+}
+
+/// returns true if an update was made, false if not
+fn update_dependency_version<'doc>(
+    manifest: &'doc mut Document,
+    package_name: &str,
+    new_version: &toml_edit::Value,
+) -> bool {
+    let dependencies = &mut manifest["dependencies"].as_table_mut().unwrap();
+
+    if !dependencies.contains_key(package_name) {
+        // it's not a direct dependency
+        return false;
+    }
+
+    let dep_version: &mut toml_edit::Value =
+        if let Some(dep) = dependencies[&package_name].as_table_mut() {
+            if dep.contains_key("version") {
+                dep["version"].as_value_mut().unwrap()
+            } else {
+                // it's a table but it only specifies path or something else weird
+                return false;
+            }
+        } else {
+            if let Some(dep) = dependencies[&package_name].as_value_mut() {
+                if let Some(inline) = dep.as_inline_table_mut() {
+                    if let Some(version) = inline.get_mut("version") {
+                        version
+                    } else {
+                        // it's an inline table but it only specifies path or something else weird
+                        return false;
+                    }
+                } else {
+                    dep
+                }
+            } else {
+                // its not a table, not an inline table, and not a value...???
+                return false;
+            }
+        };
+
+    *dep_version = new_version.clone();
+
+    true
 }
 
 fn prompt_for_new_version(krate: &Package) -> Result<Version, Error> {
