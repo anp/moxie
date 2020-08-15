95d5f3052beaa254799b1394c42f3a0cfdaf068f
Adam Perry <lol@anp.lol>
2020-08-15 12:18:57 -0700

    [todo] add compliance tests from cypress' todomvc example

---
+++ dom/examples/todo/cypress/support/index.js
@@ -0,0 +1,13 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your other test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can read more here:
+// https://on.cypress.io/guides/configuration#section-global
+// ***********************************************************
+
+require('./commands')
+require('cypress-axe')
