451a0efee2c8785932717a54adf2134045b7b140
Adam Perry <lol@anp.lol>
2020-09-04 15:50:25 -0700

    [ts-bindgen] identify which type variants we need to support for vscode

--- dom/ts-bindgen/src/typescript/ty.rs
+++ dom/ts-bindgen/src/typescript/ty.rs
@@ -20,20 +20,12 @@ impl From<TsType> for Ty {
                 println!("TODO keyword types");
                 Ty::any()
             }
-            TsType::TsThisType(_) => {
-                println!("TODO handle `this` in type annotation??");
-                Ty::any()
-            }
             TsType::TsFnOrConstructorType(_) => {
                 println!("TODO function or ctor type in annotation");
                 Ty::any()
             }
             TsType::TsTypeRef(r) => {
                 println!("TODO type reference in annotations");
-                Ty::any()
-            }
-            TsType::TsTypeQuery(q) => {
-                println!("TODO type queries in annotations");
                 Ty::any()
             }
             TsType::TsTypeLit(l) => {
@@ -48,54 +40,23 @@ impl From<TsType> for Ty {
                 println!("TODO tuple types in annotations");
                 Ty::any()
             }
-            TsType::TsOptionalType(o) => {
-                println!("TODO optional types in annotations");
-                Ty::any()
-            }
-            TsType::TsRestType(r) => {
-                println!("TODO rest types in annotations");
-                Ty::any()
-            }
             TsType::TsUnionOrIntersectionType(u) => {
                 println!("TODO union/intersect types in annotations");
                 Ty::any()
             }
-            TsType::TsConditionalType(c) => {
-                println!("TODO conditional types in annotations");
-                Ty::any()
-            }
-            TsType::TsInferType(i) => {
-                println!("TODO inferred types in annotations");
-                Ty::any()
-            }
-            TsType::TsParenthesizedType(p) => {
-                println!("TODO paren'sized types in annotations");
-                Ty::any()
-            }
-            TsType::TsTypeOperator(o) => {
-                println!("TODO type operators in annotations");
-                Ty::any()
-            }
-            TsType::TsIndexedAccessType(i) => {
-                println!("TODO indexed access types in annotations");
-                Ty::any()
-            }
-            TsType::TsMappedType(m) => {
-                println!("TODO mapped types in annotations");
-                Ty::any()
-            }
-            TsType::TsLitType(l) => {
-                println!("TODO literal types in annotations");
-                Ty::any()
-            }
-            TsType::TsTypePredicate(p) => {
-                println!("TODO predicates in annotations");
-                Ty::any()
-            }
-            TsType::TsImportType(i) => {
-                println!("TODO import types in annotations");
-                Ty::any()
-            }
+            TsType::TsOptionalType(o) => todo!("optional types"),
+            TsType::TsRestType(r) => todo!("rest types"),
+            TsType::TsTypeQuery(q) => todo!("type queries"),
+            TsType::TsThisType(_) => todo!("`this`"),
+            TsType::TsConditionalType(c) => todo!("conditional types"),
+            TsType::TsInferType(i) => todo!("inferred types"),
+            TsType::TsParenthesizedType(p) => todo!("paren'sized types"),
+            TsType::TsTypeOperator(o) => todo!("type operators"),
+            TsType::TsIndexedAccessType(i) => todo!("indexed access types"),
+            TsType::TsMappedType(m) => todo!("mapped types"),
+            TsType::TsLitType(l) => todo!("literal types"),
+            TsType::TsTypePredicate(p) => todo!("predicates"),
+            TsType::TsImportType(i) => todo!("import types"),
         }
     }
 }
