1393d76d6a4f703e0b727f8a0dc47d910d934d33
Adam Perry <lol@anp.lol>
2020-09-13 13:57:33 -0700

    [todomvc] add first real test

--- dom/examples/todo/src/integration_tests.rs
+++ dom/examples/todo/src/integration_tests.rs
@@ -1,14 +1,32 @@
-use moxie_dom::{prelude::*, raw::Node};
+use moxie_dom::{
+    prelude::*,
+    raw::{
+        testing::{Query, TargetExt},
+        Node,
+    },
+};
+use std::ops::Deref;
 use tracing::*;
 use wasm_bindgen_test::wasm_bindgen_test;
 
 #[wasm_bindgen_test]
 pub async fn add_2_todos() {
     let test = Test::new();
+    test.add_todo("learn testing").await;
+    test.add_todo("be cool").await;
+    // TODO assert .todo-list has two <li> in the right order
 }
 
 struct Test {
     root: Node,
+}
+
+impl Deref for Test {
+    type Target = Node;
+
+    fn deref(&self) -> &Self::Target {
+        &self.root
+    }
 }
 
 impl Test {
@@ -28,6 +46,18 @@ impl Test {
         super::boot(root.expect_concrete().clone());
         Test { root }
     }
+
+    async fn add_todo(&self, todo: &str) {
+        self.input().keyboardln(todo);
+        // wait for it to show up
+        // TODO make sure it shows up at the end
+        // TODO assert there's only one matching <li>
+        self.find().by_text(todo).until().many().await.unwrap();
+    }
+
+    fn input(&self) -> Node {
+        self.find().by_placeholder_text(INPUT_PLACEHOLDER).one().unwrap()
+    }
 }
 
 impl Drop for Test {
@@ -38,3 +68,5 @@ impl Drop for Test {
         // TODO clear local storage
     }
 }
+
+const INPUT_PLACEHOLDER: &str = "What needs to be done?";
