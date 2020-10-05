2678bb8e0797959d17aba9f695821cafcea0557e
Adam Perry <lol@anp.lol>
2020-10-04 18:43:07 -0700

    [dom] Add 2 versions of an example counter.

---
+++ dom/examples/counter_fn/src/lib.rs
@@ -0,0 +1,18 @@
+use mox::mox;
+use moxie_dom::{elements::html::*, prelude::*};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn boot(root: moxie_dom::raw::sys::Node) {
+    moxie_dom::boot(root, || {
+        let (count, incrementer) = state(|| 0);
+        let decrementer = incrementer.clone();
+        mox! {
+            <div>
+                <button onclick={move |_| decrementer.mutate(|count| *count -= 1)}>"-"</button>
+                {% "{}", count }
+                <button onclick={move |_| incrementer.mutate(|count| *count += 1)}>"+"</button>
+            </div>
+        }
+    });
+}
