92536d17c9bbcd398c76f539b20d860f217f8762
Adam Perry <lol@anp.lol>
2020-10-04 18:40:57 -0700

    [dom] Traits for apps to generate short JS entrypoints.

--- dom/src/lib.rs
+++ dom/src/lib.rs
@@ -17,7 +17,7 @@ pub mod text;
 pub mod prelude {
     #[cfg(feature = "webdom")]
     pub use crate::raw::sys;
-    pub use crate::raw::{document, event, Dom as _};
+    pub use crate::raw::{document, event, Dom as _, Node};
     pub use moxie::{cache, cache_state, cache_with, once, once_with, state, Key};
 
     pub use crate::{
@@ -36,8 +36,63 @@ pub mod prelude {
             node::{Node as _, Parent as _},
         },
         text::text,
+        Stateful,
     };
 }
+
+use std::fmt::Debug;
+
+/// A stateful element within the application.
+// TODO make a way for the invoker to pass extra args instead of default?
+pub trait Stateful: Debug + Sized + 'static {
+    /// The value returned from `update` on each revision.
+    type Output: interfaces::node::Child;
+
+    /// The type used to generate updates to the app, typically a wrapper around
+    /// [`moxie::Key`]s.
+    type Updater: From<moxie::Key<Self>>;
+
+    /// Compute a new version of the output.
+    fn tick(&self, updater: Self::Updater) -> Self::Output;
+}
+
+/// A "root" stateful element which can be booted directly without any
+/// arguments.
+pub trait Boot: Stateful + Default {
+    /// Start the app running with the provided `root`.
+    fn boot(root: impl Into<prelude::Node>) {
+        boot(root, || {
+            let (app, updater) = prelude::state(Self::default);
+            app.tick(updater.into())
+        });
+    }
+}
+
+/// Produce an interactive entrypoint for the specified app type. Creates a
+/// `#[wasm_bindgen]` export with the name of the app type prefixed with `boot`.
+/// For example, `app_boot!(Example)` would export a JavaScript function named
+/// `bootExample`.
+#[macro_export]
+macro_rules! app_boot {
+    ($app:ty) => {
+        moxie_dom::__paste! {
+            impl moxie_dom::Boot for $app {}
+
+            #[moxie_dom::__wasm_bindgen(js_name = [<boot $app>])]
+            #[doc(hidden)]
+            pub fn [<__js_boot_ $app:snake>] (root: moxie_dom::raw::sys::Node) {
+                <$app as moxie_dom::Boot>::boot(root);
+            }
+        }
+    };
+}
+
+#[cfg(feature = "webdom")]
+#[doc(hidden)]
+pub use wasm_bindgen::prelude::wasm_bindgen as __wasm_bindgen;
+
+#[doc(hidden)]
+pub use paste::paste as __paste;
 
 /// Provides the underlying DOM implementation for moxie-dom.
 pub use augdom as raw;
