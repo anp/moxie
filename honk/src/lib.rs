10f7dd112bf70dbac209d8545d7d499ea88f4257
Adam Perry <lol@anp.lol>
2020-11-03 12:28:47 -0800

    [honk] stub out maintain/converge distinction

--- honk/src/lib.rs
+++ honk/src/lib.rs
@@ -1,22 +1,36 @@
 use color_eyre::eyre::Result;
 use std::path::{Path, PathBuf};
-use tracing::info;
+use tracing::{info, instrument, warn};
 
 #[derive(Debug)]
 pub struct Workspace {
-    /// Root directory of the workspace.
+    /// Path to `workspace.honk`.
     root: PathBuf,
 }
 
 impl Workspace {
-    pub fn open(root: impl AsRef<Path>) -> Result<Self> {
-        let root = root.as_ref().to_path_buf();
-        info!(root = %root.display(), "Opening.");
-        Ok(Self { root })
+    pub fn new(root: impl AsRef<Path>) -> Self {
+        Self { root: root.as_ref().to_path_buf() }
     }
 
-    pub fn run(self) -> Result<()> {
-        info!(root = %self.root.display(), "Running.");
+    pub fn maintain(self) -> Result<()> {
+        info!("maintaining workspace");
+        loop {
+            self.converge()?;
+            warn!("TODO wait for changes to inputs");
+            std::thread::sleep(std::time::Duration::from_secs(10));
+        }
+    }
+
+    #[instrument(level = "info", skip(self), fields(root = %self.root.display()))]
+    fn converge(&self) -> Result<()> {
+        info!("reading workspace file");
+
+        warn!("TODO get workspace file contents");
+        warn!("TODO evaluate workspace file");
+        warn!("TODO run formatters");
+        warn!("TODO run build/test");
+
         Ok(())
     }
 }
--- honk/src/main.rs
+++ honk/src/main.rs
@@ -5,14 +5,18 @@ use std::path::PathBuf;
 /// An awful billed system.
 #[derive(Debug, FromArgs)]
 struct HonkCli {
-    /// path to the workspace root.
-    #[argh(option, default = "std::env::current_dir().unwrap()")]
+    /// path to `workspace.honk`.
+    #[argh(option, default = "default_workspace_path()")]
     workspace: PathBuf,
 }
 
+fn default_workspace_path() -> PathBuf {
+    std::env::current_dir().unwrap().join("workspace.honk")
+}
+
 fn main() -> color_eyre::eyre::Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt::fmt().pretty().init();
     color_eyre::install()?;
     let HonkCli { workspace } = argh::from_env();
-    Workspace::open(workspace)?.run()
+    Workspace::new(workspace).maintain()
 }
