aa60394739224d80160116b91db7e96fa3ea730e
Adam Perry <lol@anp.lol>
2020-11-22 12:12:28 -0800

    [honk] capture spantraces

--- honk/src/main.rs
+++ honk/src/main.rs
@@ -1,6 +1,7 @@
 use argh::FromArgs;
 use honk::Workspace;
 use std::path::PathBuf;
+use tracing_subscriber::Layer;
 
 /// An awful billed system.
 #[derive(Debug, FromArgs)]
@@ -11,7 +12,9 @@ struct HonkCli {
 }
 
 fn main() -> color_eyre::eyre::Result<()> {
-    tracing_subscriber::fmt::fmt().pretty().init();
+    let subscriber = tracing_error::ErrorLayer::default()
+        .with_subscriber(tracing_subscriber::fmt::fmt().pretty().finish());
+    tracing::subscriber::set_global_default(subscriber)?;
     color_eyre::install()?;
     let HonkCli { workspace } = argh::from_env();
     Workspace::new(workspace).maintain()
