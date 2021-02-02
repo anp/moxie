a5495a1ea934e8e8793d2a52c4a686d468d918cd
Adam Perry <lol@anp.lol>
2021-02-01 16:17:56 -0800

    [dom] Fix attributes which disappear on change.
    Confirmed locally that the test fails before the actual patch.
    
    Resolves #244.

--- dom/src/cached_node.rs
+++ dom/src/cached_node.rs
@@ -31,15 +31,28 @@ impl CachedNode {
     // TODO accept PartialEq+ToString implementors
     #[topo::nested(slot = "&(self.id, name)")]
     pub(crate) fn set_attribute(&self, name: &'static str, value: &str) {
+        let mut should_set = false;
         cache_with(
             value,
-            |v| {
-                self.node.set_attribute(name, v);
+            |_| {
+                // when this isn't the first time the attribute is being set for this element,
+                // this closure executes while the previous attribute's guard is still live.
+                // if we actually set the attribute here, it will be removed when this closure exits
+                // which we definitely don't want. easiest fix is to set the attribute after our
+                // hypothetical cleanup has completed
+                should_set = true;
                 let name = name.to_owned();
-                scopeguard::guard(self.node.clone(), move |node| node.remove_attribute(&name))
+                // TODO find a way to reuse the guard if we're replacing a previous value
+                scopeguard::guard(self.node.clone(), move |node| {
+                    node.remove_attribute(&name);
+                })
             },
             |_| {},
         );
+
+        if should_set {
+            self.node.set_attribute(name, value);
+        }
     }
 
     pub(crate) fn ensure_child_attached(&self, new_child: &Node) {
@@ -84,5 +97,32 @@ impl CachedNode {
 impl Debug for CachedNode {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         f.debug_struct("CachedNode").field("node", &self.node).finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{elements::just_all_of_it_ok::div, prelude::*};
+    use mox::mox;
+    use moxie::{runtime::RunLoop, state};
+
+    #[wasm_bindgen_test::wasm_bindgen_test]
+    pub fn attributes_change() {
+        let mut rt = RunLoop::new(|| {
+            let (value, key) = state(|| String::from("boo"));
+            (key, mox!(<div id=&*value/>))
+        });
+        let (key, node) = rt.run_once();
+        assert_eq!(
+            node.raw_node_that_has_sharp_edges_please_be_careful().to_string(),
+            "<div id=\"boo\">\n</div>"
+        );
+
+        key.set(String::from("aha"));
+        let (_, node) = rt.run_once();
+        assert_eq!(
+            node.raw_node_that_has_sharp_edges_please_be_careful().to_string(),
+            "<div id=\"aha\">\n</div>"
+        );
     }
 }
