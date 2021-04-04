1e5b13830c335992caae61fbfb29eb42a22b4022
Adam Perry <lol@anp.lol>
2021-04-04 08:29:29 -0700

    [honk] Order tests based on dep graph.

--- honk/src/builtins/target.rs
+++ honk/src/builtins/target.rs
@@ -1,9 +1,9 @@
 use crate::{builtins::command::RefHonkCommand, EvaluatorExt};
-use starlark::{environment::GlobalsBuilder, values::none::NoneType};
+use starlark::{environment::GlobalsBuilder, values::{Value, none::NoneType}};
 
 #[starlark_module::starlark_module]
 pub fn register(globals: &mut GlobalsBuilder) {
-    fn target(name: &str, command: RefHonkCommand) -> NoneType {
+    fn target(name: &str, command: RefHonkCommand, deps: Option<Vec<Value<'_>>>) -> NoneType {
         ctx.revision().register_target(name, command);
         Ok(NoneType)
     }
