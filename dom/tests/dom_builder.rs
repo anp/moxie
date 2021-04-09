926441a4ecacfa46b593d0549b3adac0571f96c5
Simon Bourne <simonbourne@gmail.com>
2021-04-09 15:43:12 +0100

    cargo ofl fmt

--- dom/tests/dom_builder.rs
+++ dom/tests/dom_builder.rs
@@ -1,35 +1,39 @@
 use mox::mox;
-use moxie_dom::{elements::html::div, interfaces::node::{Child, NodeBuilder}, prelude::*};
+use moxie_dom::{
+    elements::html::div,
+    interfaces::node::{Child, NodeBuilder},
+    prelude::*,
+};
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
-pub async fn render_test<Root>(render_child: impl FnMut() -> Root + 'static, expected: &str) where Root: Child + 'static{
+pub async fn render_test<Root>(render_child: impl FnMut() -> Root + 'static, expected: &str)
+where
+    Root: Child + 'static,
+{
     let test_root = document().create_element("div");
     moxie_dom::boot(test_root.clone(), render_child);
 
-    assert_eq!(
-        test_root.first_child().unwrap().to_string(),
-        expected,
-    );
+    assert_eq!(test_root.first_child().unwrap().to_string(), expected,);
 }
 
 #[wasm_bindgen_test]
 pub async fn simple_mox() {
-    render_test(|| mox!(<div>"test text"</div>),"<div>test text</div>").await;
+    render_test(|| mox!(<div>"test text"</div>), "<div>test text</div>").await;
 }
 
 #[wasm_bindgen_test]
 pub async fn block_mox() {
-    render_test(|| mox!(<div>{"test text"}</div>),"<div>test text</div>").await;
+    render_test(|| mox!(<div>{"test text"}</div>), "<div>test text</div>").await;
 }
 
 #[wasm_bindgen_test]
 pub async fn simple_builder() {
-    render_test(|| div().child(text("test text")),"<div>test text</div>").await;
+    render_test(|| div().child(text("test text")), "<div>test text</div>").await;
 }
 
 #[wasm_bindgen_test]
 pub async fn built_builder() {
-    render_test(|| div().child(text("test text").build()).build(),"<div>test text</div>").await;
+    render_test(|| div().child(text("test text").build()).build(), "<div>test text</div>").await;
 }
